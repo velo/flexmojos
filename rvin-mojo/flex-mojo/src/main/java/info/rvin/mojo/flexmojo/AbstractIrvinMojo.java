fa0979feb6fc1a6003f20355558052d36d84fa5e
Marvin Froeder <velo.br@gmail.com>
2008-03-19 01:37:46 +0000

    First source commit

---
+++ rvin-mojo/flex-mojo/src/main/java/info/rvin/mojo/flexmojo/AbstractIrvinMojo.java
@@ -0,0 +1,140 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.rvin.mojo.flexmojo;
+
+import java.util.List;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+
+/**
+ * 
+ * Encapsulate the access to Maven API. Some times just to hide Java 5 warnings
+ * 
+ * @author tech35212
+ * 
+ */
+public abstract class AbstractIrvinMojo extends AbstractMojo {
+
+	/**
+	 * The maven project.
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	protected MavenProject project;
+
+	/**
+	 * @parameter expression="${project.build}"
+	 * @required
+	 * @readonly
+	 */
+	protected Build build;
+
+	/**
+	 * @component
+	 */
+	protected MavenProjectHelper projectHelper;
+
+	/**
+	 * @component
+	 */
+	protected ArtifactFactory artifactFactory;
+
+	/**
+	 * @component
+	 */
+	protected ArtifactResolver resolver;
+
+	/**
+	 * @parameter expression="${localRepository}"
+	 */
+	protected ArtifactRepository localRepository;
+
+	/**
+	 * @parameter expression="${project.remoteArtifactRepositories}"
+	 */
+	@SuppressWarnings("unchecked")
+	protected List remoteRepositories;
+
+	public AbstractIrvinMojo() {
+		super();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected List<Resource> getResources() {
+		// I wanna maven on Java 5
+		return build.getResources();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected List<Dependency> getDependencies() {
+		return project.getDependencies();
+	}
+
+	protected Artifact getArtifact(Dependency dependency)
+			throws MojoExecutionException {
+		Artifact artifact = artifactFactory.createArtifactWithClassifier(
+				dependency.getGroupId(), dependency.getArtifactId(), dependency
+						.getVersion(), dependency.getType(), dependency
+						.getClassifier());
+		resolveArtifact(artifact);
+		return artifact;
+	}
+
+	protected void resolveArtifact(Artifact artifact)
+			throws MojoExecutionException {
+		try {
+			resolver.resolve(artifact, remoteRepositories, localRepository);
+		} catch (ArtifactResolutionException e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		} catch (ArtifactNotFoundException e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
+	}
+
+	public final void execute() throws MojoExecutionException,
+			MojoFailureException {
+		setUp();
+		run();
+		tearDown();
+	}
+
+	protected abstract void setUp() throws MojoExecutionException,
+			MojoFailureException;
+
+	protected abstract void run() throws MojoExecutionException,
+			MojoFailureException;
+
+	protected abstract void tearDown() throws MojoExecutionException,
+			MojoFailureException;
+
+}
---
+++ rvin-mojo/flex-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/Font.java
@@ -0,0 +1,106 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.rvin.mojo.flexmojo.compiler;
+
+import java.io.File;
+
+public class Font {
+
+    /**
+     * Enables advanced anti-aliasing for embedded fonts, which provides greater
+     * clarity for small fonts. This setting can be overriden in CSS for
+     * specific fonts.
+     */
+    private boolean advancedAntiAliasing = true;
+
+    /**
+     * The number of embedded font faces that are cached.
+     */
+    private int maxCachedFonts = 20;
+
+    /**
+     * The number of character glyph outlines to cache for each font face.
+     */
+    private int maxGlyphsPerFace = 1000;
+
+    /**
+     * Defines ranges that can be used across multiple font-face declarations.
+     */
+    private String[] languages;
+
+    /**
+     * Compiler font manager classes, in policy resolution order
+     */
+    private String[] managers;
+
+    /**
+     * File containing cached system font licensing information produced via
+     * java -cp mxmlc.jar flex2.tools.FontSnapshot (fontpath) Will default to
+     * winFonts.ser on Windows XP and macFonts.ser on Mac OS X, so is commented
+     * out by default.
+     */
+    private File localFontsSnapshot;
+
+    public boolean isAdvancedAntiAliasing() {
+        return advancedAntiAliasing;
+    }
+
+    public void setAdvancedAntiAliasing(boolean advancedAntiAliasing) {
+        this.advancedAntiAliasing = advancedAntiAliasing;
+    }
+
+    public int getMaxCachedFonts() {
+        return maxCachedFonts;
+    }
+
+    public void setMaxCachedFonts(int maxCachedFonts) {
+        this.maxCachedFonts = maxCachedFonts;
+    }
+
+    public int getMaxGlyphsPerFace() {
+        return maxGlyphsPerFace;
+    }
+
+    public void setMaxGlyphsPerFace(int maxGlyphsPerFace) {
+        this.maxGlyphsPerFace = maxGlyphsPerFace;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public String[] getManagers() {
+        return managers;
+    }
+
+    public void setManagers(String[] managers) {
+        this.managers = managers;
+    }
+
+    public File getLocalFontsSnapshot() {
+        return localFontsSnapshot;
+    }
+
+    public void setLocalFontsSnapshot(File localFontsSnapshot) {
+        this.localFontsSnapshot = localFontsSnapshot;
+    }
+}
