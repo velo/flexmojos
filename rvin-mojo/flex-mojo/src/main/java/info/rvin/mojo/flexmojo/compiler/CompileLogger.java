78e83e412e0047b5e613f7ebb990bcc9f751de4d
Marvin Froeder <velo.br@gmail.com>
2008-03-22 15:07:57 +0000

    Identation

--- rvin-mojo/flex-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/CompileLogger.java
+++ rvin-mojo/flex-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/CompileLogger.java
@@ -7,22 +7,20 @@ import flex2.tools.oem.Message;
 
 public class CompileLogger implements Logger {
 
-    private Log log;
+	private Log log;
 
-    public CompileLogger(Log log) {
-	this.log = log;
-    }
-
-    public void log(Message msg, int errorCode, String source) {
-	if (Message.ERROR.equals(msg.getLevel())) {
-	    log.error(msg.toString());
-	} else 
-	if (Message.INFO.equals(msg.getLevel())) {
-	    log.info(msg.toString());
-	} else 
-	if (Message.WARNING.equals(msg.getLevel())) {
-	    log.warn(msg.toString());
+	public CompileLogger(Log log) {
+		this.log = log;
 	}
-    }
+
+	public void log(Message msg, int errorCode, String source) {
+		if (Message.ERROR.equals(msg.getLevel())) {
+			log.error(msg.toString());
+		} else if (Message.INFO.equals(msg.getLevel())) {
+			log.info(msg.toString());
+		} else if (Message.WARNING.equals(msg.getLevel())) {
+			log.warn(msg.toString());
+		}
+	}
 
 }
