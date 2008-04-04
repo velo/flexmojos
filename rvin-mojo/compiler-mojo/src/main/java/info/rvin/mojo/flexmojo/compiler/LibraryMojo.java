db8178d253b857314d90a4300c54c011481e0d63
Marvin Froeder <velo.br@gmail.com>
2008-04-04 01:25:22 +0000

    Improve l10n support

--- rvin-mojo/compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/LibraryMojo.java
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/LibraryMojo.java
@@ -36,6 +36,8 @@ package info.rvin.mojo.flexmojo.compiler;
 import static info.rvin.flexmojos.utilities.MavenUtils.resolveArtifact;
 import flex2.tools.oem.Library;
 
+import info.rvin.flexmojos.utilities.MavenUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -167,7 +169,8 @@ public class LibraryMojo extends AbstractFlexCompilerMojo<Library> {
 								mvnArtifact.getArtifactId(), mvnArtifact
 										.getVersion(), "properties",
 								"resource-bundle");
-				resolveArtifact(artifact, resolver, localRepository, remoteRepositories);
+				resolveArtifact(artifact, resolver, localRepository,
+						remoteRepositories);
 				String bundleFile;
 				try {
 					bundleFile = FileUtils.readFileToString(artifact.getFile());
@@ -219,11 +222,15 @@ public class LibraryMojo extends AbstractFlexCompilerMojo<Library> {
 			return;
 		}
 		for (String locale : locales) {
-			String path = resourceBundlePath.replace("{locale}", locale);
-			File localePath = new File(path);
+			getLog().info("Generating resource-bundle for " + locale);
+			File localePath = MavenUtils.getLocaleResourcePath(
+					resourceBundlePath, locale);
+
 			if (!localePath.exists()) {
-				throw new MojoExecutionException(
-						"Unable to find locales path: " + path);
+				getLog().error(
+						"Unable to find locales path: "
+								+ localePath.getAbsolutePath());
+				continue;
 			}
 
 			Library localized = new Library();
