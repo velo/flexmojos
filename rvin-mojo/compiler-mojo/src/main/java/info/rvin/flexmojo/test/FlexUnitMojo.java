aa94db3ac2c6d9bbd65cdd065450674db357a154
Marvin Froeder <velo.br@gmail.com>
2008-04-08 01:32:29 +0000

    Preparing flexunit release

--- rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/FlexUnitMojo.java
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/FlexUnitMojo.java
@@ -48,7 +48,7 @@ public class FlexUnitMojo extends AbstractIrvinMojo {
 	private int port = 1024;
 	private int socketTimeout = 60000; // milliseconds
 	private boolean failOnTestFailure = true;
-	private File swf = new File("D:/flex/workspace/rvin-mojo/flex-mojo-IT/src/test/resources/flexunit-example/target/test-classes/TestRunner.swf");
+	private File swf;
 
 	private MojoExecutionException executionError; // BAD IDEA
 
@@ -57,6 +57,9 @@ public class FlexUnitMojo extends AbstractIrvinMojo {
 	 */
 	@Override
 	protected void setUp() throws MojoExecutionException, MojoFailureException {
+		
+		swf = new File(build.getTestOutputDirectory(), "TestRunner.swf");
+		
 		// Start a thread that receives the FlexUnit results.
 		receiveFlexUnitResults();
 	}
@@ -241,7 +244,7 @@ public class FlexUnitMojo extends AbstractIrvinMojo {
 				log(formatLogReport(root));
 
 			// Get the output file name.
-			final File file = new File("D:/flex/workspace/rvin-mojo/flex-mojo-IT/src/test/resources/flexunit-example/target/test-classes/Report.xml");
+			final File file = new File(build.getTestOutputDirectory(), "TestReport.xml");
 
 			// Pretty print the document to disk.
 			final OutputFormat format = OutputFormat.createPrettyPrint();
