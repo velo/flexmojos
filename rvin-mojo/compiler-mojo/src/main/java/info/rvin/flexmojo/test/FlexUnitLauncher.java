61a888351f044d9fdae5cfbe67ec057cd3763a65
Marvin Froeder <velo.br@gmail.com>
2008-04-08 23:45:05 +0000

    Applied patch from issue 1.

--- rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/FlexUnitLauncher.java
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/FlexUnitLauncher.java
@@ -8,6 +8,8 @@ import java.io.File;
 public class FlexUnitLauncher {
 	private static final String WINDOWS_OS = "Windows";
 	private static final String WINDOWS_CMD = "rundll32 url.dll,FileProtocolHandler ";
+	private static final String MAC_OS = "Mac OS X";
+	private static final String MAC_CMD = "open ";
 	private static final String UNIX_CMD = "gflashplayer ";
 
 	/**
@@ -24,6 +26,11 @@ public class FlexUnitLauncher {
 			// it, not so easy in a browser. We let 'rundll32' do the work based
 			// on the extension of the file passed in.
 			Runtime.getRuntime().exec(WINDOWS_CMD + swf.getAbsolutePath());
+		} else if (isMac()) {
+			// Ideally we want to launch the SWF in the player so we can close
+			// it, not so easy in a browser. We let 'open' do the work based
+			// on the extension of the file passed in.
+			Runtime.getRuntime().exec(MAC_CMD + swf.getAbsolutePath());
 		} else {
 			// If we are running in UNIX the fallback is to the browser. To do
 			// this Netscape must be running for the "-remote" flag to work. If
@@ -50,6 +57,21 @@ public class FlexUnitLauncher {
 		String os = System.getProperty("os.name");
 
 		if (os.startsWith(WINDOWS_OS)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Return a boolean to show if we are running on Mac OS X.
+	 * 
+	 * @return true if we are running on Mac OS X.
+	 */
+	private boolean isMac() {
+		String os = System.getProperty("os.name");
+
+		if (os.startsWith(MAC_OS)) {
 			return true;
 		} else {
 			return false;
