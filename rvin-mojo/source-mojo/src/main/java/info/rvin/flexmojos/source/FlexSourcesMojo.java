f966b424eaf376f12417edcadd7d7500d2b8f5b7
Marvin Froeder <velo.br@gmail.com>
2008-04-11 00:17:31 +0000

    first commit

---
+++ rvin-mojo/source-mojo/src/main/java/info/rvin/flexmojos/source/FlexSourcesMojo.java
@@ -0,0 +1,14 @@
+package info.rvin.flexmojos.source;
+
+import org.apache.maven.plugin.source.SourceJarMojo;
+
+/**
+ * @extendsPlugin source
+ * @extendsGoal jar
+ * 
+ * @goal jar
+ * @phase package
+ */
+public class FlexSourcesMojo extends SourceJarMojo {
+
+}
