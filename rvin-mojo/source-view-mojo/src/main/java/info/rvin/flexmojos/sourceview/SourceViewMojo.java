5331ebc8673cb55e0544b57e710c715a764b81c1
Marvin Froeder <velo.br@gmail.com>
2008-04-12 03:29:31 +0000

    First steps on source-view mojo

---
+++ rvin-mojo/source-view-mojo/src/main/java/info/rvin/flexmojos/sourceview/SourceViewMojo.java
@@ -0,0 +1,73 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.rvin.flexmojos.sourceview;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.w3c.dom.html.HTMLCollection;
+
+import de.java2html.Java2Html;
+import de.java2html.util.HtmlUtilities;
+
+/**
+ * @goal source-view
+ * @phase package
+ */
+public class SourceViewMojo extends AbstractMojo {
+
+	/**
+	 * The maven project.
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	protected MavenProject project;
+
+	@SuppressWarnings("unchecked")
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		File srcDir = new File(project.getBuild().getSourceDirectory());
+		Collection<File> testFiles = FileUtils.listFiles(srcDir, new String[]{"as"}, true);
+		for (File file : testFiles) {
+			String fileContent;
+			try {
+				fileContent = IOUtils.toString(new FileReader(file));
+			} catch (IOException e) {
+				throw new MojoExecutionException(e.getMessage(), e);
+			}
+			System.out.println(fileContent);
+			fileContent = Java2Html.convertToHtmlPage(fileContent);
+			System.out.println(fileContent);
+			System.exit(0);
+		}
+	}
+
+}
