8bc6b2300ac9ef6438ec1b4b135bbfe6ef69e371
Marvin Froeder <velo.br@gmail.com>
2008-04-17 01:49:37 +0000

    IT for issue 13

--- rvin-mojo/flex-mojo-IT/src/test/java/info/rvin/mojo/flexmojo/test/IT0007Issue0013Test.java
+++ rvin-mojo/flex-mojo-IT/src/test/java/info/rvin/mojo/flexmojo/test/IT0007Issue0013Test.java
@@ -1,8 +1,6 @@
 package info.rvin.mojo.flexmojo.test;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.maven.integrationtests.AbstractMavenIntegrationTestCase;
 import org.apache.maven.it.Verifier;
@@ -16,15 +14,12 @@ public class IT0007Issue0013Test extends AbstractMavenIntegrationTestCase {
 		Verifier verifier = new Verifier(testDir.getAbsolutePath(), true);
 		verifier.deleteArtifact("info.rvin.itest.issues", "issue-0013",
 				"1.0-SNAPSHOT", "swf");
-		List<String> cliOptions = new ArrayList<String>();
-		cliOptions.add("-o");
-		verifier.setCliOptions(cliOptions );
 		verifier.executeGoal("install");
 		verifier.displayStreamBuffers();
-//		verifier.verifyErrorFreeLog();
+		verifier.verifyErrorFreeLog();
 		verifier.resetStreams();
 		
-		File reportDir = new File(testDir, "target/tests-reports");
+		File reportDir = new File(testDir, "target/test-reports");
 		assertEquals(2, reportDir.listFiles().length);
 	}
 
