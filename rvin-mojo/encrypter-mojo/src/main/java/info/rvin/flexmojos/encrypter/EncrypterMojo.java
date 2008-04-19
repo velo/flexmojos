8ffcc634989daf578c02601b0751d00ff4e8f600
Marvin Froeder <velo.br@gmail.com>
2008-04-19 05:10:07 +0000

    Begin encryption support

---
+++ rvin-mojo/encrypter-mojo/src/main/java/info/rvin/flexmojos/encrypter/EncrypterMojo.java
@@ -0,0 +1,14 @@
+package info.rvin.flexmojos.encrypter;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+public class EncrypterMojo extends AbstractMojo {
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
