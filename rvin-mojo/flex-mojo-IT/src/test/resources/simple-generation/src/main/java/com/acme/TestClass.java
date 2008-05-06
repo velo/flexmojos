005f0010ccb0931b25c45e49a10a2449c8f3c7df
Marvin Froeder <velo.br@gmail.com>
2008-05-06 01:02:23 +0000

    Generator test application

---
+++ rvin-mojo/flex-mojo-IT/src/test/resources/simple-generation/src/main/java/com/acme/TestClass.java
@@ -0,0 +1,14 @@
+package com.acme;
+
+public class TestClass {
+
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
