110d2e48b800067326f2a17fba8d80daeccd2208
Marvin Froeder <velo.br@gmail.com>
2008-05-14 01:54:54 +0000

    Add some missed parameters

---
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/Metadata.java
@@ -0,0 +1,15 @@
+package info.rvin.mojo.flexmojo.compiler;
+
+public class Metadata {
+
+	private String contributor;
+	private String creator;
+	private String date;
+	private String description;
+	private String language;
+	private String localizedDescription;
+	private String localizedTitle;
+	private String publisher;
+	private String title;
+
+}
