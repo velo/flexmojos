6e4b4f49572a3a0a43517c7ad3e1807812d9e5dd
Joost den Boer <jdboer@e-id.nl>
2008-05-15 00:16:37 +0000

    Added class comment

--- rvin-mojo/source-mojo/src/main/java/info/rvin/flexmojos/source/FlexSourcesMojo.java
+++ rvin-mojo/source-mojo/src/main/java/info/rvin/flexmojos/source/FlexSourcesMojo.java
@@ -3,6 +3,9 @@ package info.rvin.flexmojos.source;
 import org.apache.maven.plugin.source.SourceJarMojo;
 
 /**
+ * Goal to create a JAR-package containing all the source files of a Flex
+ * project.
+ * 
  * @extendsPlugin source
  * @extendsGoal jar
  * 
