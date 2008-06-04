d047f80c892be7da602bfcd3be5a20d6c3d3fec4
Marvin Froeder <velo.br@gmail.com>
2008-06-04 22:10:37 +0000

    Fix issue 44

--- rvin-mojo/asdoc-mojo/src/main/java/info/rvin/flexmojos/asdoc/AsDocMojo.java
+++ rvin-mojo/asdoc-mojo/src/main/java/info/rvin/flexmojos/asdoc/AsDocMojo.java
@@ -17,6 +17,7 @@
  */
 package info.rvin.flexmojos.asdoc;
 
+import flex2.compiler.util.ThreadLocalToolkit;
 import flex2.tools.ASDoc;
 import info.rvin.flexmojos.utilities.MavenUtils;
 
@@ -45,7 +46,7 @@ import org.apache.maven.project.MavenProjectHelper;
 
 /**
  * Goal which generates documentation from the ActionScript sources.
- *
+ * 
  * @goal asdoc
  * @requiresDependencyResolution
  */
@@ -53,7 +54,7 @@ public class AsDocMojo extends AbstractMojo {
 
 	/**
 	 * The maven project.
-	 *
+	 * 
 	 * @parameter expression="${project}"
 	 * @required
 	 * @readonly
@@ -111,11 +112,11 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * A list of classes to document. These classes must be in the source path.
 	 * This is the default option.
-	 *
+	 * 
 	 * This option works the same way as does the -include-classes option for
 	 * the compc component compiler. For more information, see Using compc, the
 	 * component compiler.
-	 *
+	 * 
 	 * @parameter
 	 */
 	private String[] docClasses;
@@ -123,14 +124,14 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * A list of URIs whose classes should be documented. The classes must be in
 	 * the source path.
-	 *
+	 * 
 	 * You must include a URI and the location of the manifest file that defines
 	 * the contents of this namespace.
-	 *
+	 * 
 	 * This option works the same way as does the -include-namespaces option for
 	 * the compc component compiler. For more information, see Using compc, the
 	 * component compiler.
-	 *
+	 * 
 	 * @parameter
 	 */
 	private Namespace[] docNamespaces;
@@ -138,13 +139,13 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * A list of files that should be documented. If a directory name is in the
 	 * list, it is recursively searched.
-	 *
+	 * 
 	 * This option works the same way as does the -include-sources option for
 	 * the compc component compiler. For more information, see Using compc, the
 	 * component compiler.
-	 *
+	 * 
 	 * @parameter
-	 *
+	 * 
 	 */
 	private File[] docSources;
 
@@ -152,9 +153,9 @@ public class AsDocMojo extends AbstractMojo {
 	 * A list of classes that should not be documented. You must specify
 	 * individual class names. Alternatively, if the ASDoc comment for the class
 	 * contains the
-	 *
+	 * 
 	 * @private tag, is not documented.
-	 *
+	 * 
 	 * @parameter
 	 */
 	private String[] excludeClasses;
@@ -162,9 +163,9 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * Whether all dependencies found by the compiler are documented. If true,
 	 * the dependencies of the input classes are not documented.
-	 *
+	 * 
 	 * The default value is false.
-	 *
+	 * 
 	 * @parameter default-value="false"
 	 */
 	private boolean excludeDependencies;
@@ -172,7 +173,7 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * The text that appears at the bottom of the HTML pages in the output
 	 * documentation.
-	 *
+	 * 
 	 * @parameter
 	 */
 	private String footer;
@@ -181,20 +182,20 @@ public class AsDocMojo extends AbstractMojo {
 	 * An integer that changes the width of the left frameset of the
 	 * documentation. You can change this size to accommodate the length of your
 	 * package names.
-	 *
+	 * 
 	 * The default value is 210 pixels.
-	 *
+	 * 
 	 * @parameter default-value="120"
-	 *
+	 * 
 	 */
 	private int leftFramesetWidth;
 
 	/**
 	 * The text that appears at the top of the HTML pages in the output
 	 * documentation.
-	 *
+	 * 
 	 * The default value is "API Documentation".
-	 *
+	 * 
 	 * @parameter default-value="API Documentation"
 	 */
 	private String mainTitle;
@@ -202,7 +203,7 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * The output directory for the generated documentation. The default value
 	 * is "asdoc-output".
-	 *
+	 * 
 	 * @parameter
 	 */
 	protected File output;
@@ -210,7 +211,7 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * The descriptions to use when describing a package in the documentation.
 	 * You can specify more than one package option.
-	 *
+	 * 
 	 * @parameter
 	 */
 	private Map<String, String> packageDescriptions;
@@ -220,16 +221,16 @@ public class AsDocMojo extends AbstractMojo {
 	 * asdoc/templates directory in the ASDoc installation directory. This
 	 * directory contains all the HTML, CSS, XSL, and image files used for
 	 * generating the output.
-	 *
+	 * 
 	 * @parameter
 	 */
 	private File templatesPath;
 
 	/**
 	 * The text that appears in the browser window in the output documentation.
-	 *
+	 * 
 	 * The default value is "API Documentation".
-	 *
+	 * 
 	 * @parameter default-value="API Documentation"
 	 */
 	private String windowTitle;
@@ -241,7 +242,7 @@ public class AsDocMojo extends AbstractMojo {
 
 	/**
 	 * Load a file containing configuration options
-	 *
+	 * 
 	 * @parameter
 	 */
 	private File configFile;
@@ -250,7 +251,7 @@ public class AsDocMojo extends AbstractMojo {
 
 	/**
 	 * specifies a compatibility version. e.g. compatibility 2.0.1
-	 *
+	 * 
 	 * @parameter
 	 */
 	private String compatibilityVersion;
@@ -258,7 +259,7 @@ public class AsDocMojo extends AbstractMojo {
 	/**
 	 * List of path elements that form the roots of ActionScript class
 	 * hierarchies.
-	 *
+	 * 
 	 * @parameter
 	 */
 	protected File[] sourcePaths;
@@ -416,6 +417,11 @@ public class AsDocMojo extends AbstractMojo {
 		// I hate this, waiting for asdoc-oem
 		// https://bugs.adobe.com/jira/browse/SDK-15405
 		ASDoc.asdoc(args.toArray(new String[args.size()]));
+
+		int errorCount = ThreadLocalToolkit.errorCount();
+		if (errorCount > 0) {
+			throw new MojoExecutionException("Error compiling!");
+		}
 	}
 
 	private void addDocNamespaces(List<String> args) {
