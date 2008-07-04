ef90fa2c4b61af678b45139b76c2f2a5c5fe5f7a
Marvin Froeder <velo.br@gmail.com>
2008-07-04 03:04:29 +0000

    Fixed issue 59

---
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/TestLibraryCompilerMojo.java
@@ -0,0 +1,69 @@
+package info.rvin.flexmojo.test;
+
+import info.rvin.flexmojos.utilities.MavenUtils;
+import info.rvin.mojo.flexmojo.compiler.LibraryMojo;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Goal to compile the Flex test sources.
+ *
+ * @goal test-swc
+ * @requiresDependencyResolution
+ */
+public class TestLibraryCompilerMojo extends LibraryMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		File testFolder = new File(build.getTestSourceDirectory());
+
+		if (testFolder.exists()) {
+			setUp();
+			run();
+			tearDown();
+		} else {
+			getLog().warn("Test folder not found.");
+		}
+
+	}
+
+	@Override
+	public void setUp() throws MojoExecutionException, MojoFailureException {
+		isSetProjectFile = false;
+
+		File outputFolder = new File(build.getTestOutputDirectory());
+		if (!outputFolder.exists()) {
+			outputFolder.mkdirs();
+		}
+
+		outputFile = new File(build.getDirectory(), build.getFinalName()
+				+ "-test.swc");
+
+		includeSources = MavenUtils.getTestSourcePaths(build);
+
+		super.setUp();
+	}
+
+	@Override
+	protected void configure() throws MojoExecutionException {
+		super.configure();
+
+		// add test libraries
+		configuration.addLibraryPath(getDependenciesPath("test"));
+
+		configuration.addSourcePath(MavenUtils.getTestSourcePaths(build));
+	}
+
+	@Override
+	protected void tearDown() throws MojoExecutionException,
+			MojoFailureException {
+		super.tearDown();
+
+		projectHelper.attachArtifact(project, "swc", "test", outputFile);
+
+	}
+
+}
