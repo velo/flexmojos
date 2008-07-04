283313f6f2c034fc2bc2e3999bd1f6da2be84681
Marvin Froeder <velo.br@gmail.com>
2008-07-04 03:02:53 +0000

    Organized imports

--- rvin-mojo/compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/AbstractFlexCompilerMojo.java
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/AbstractFlexCompilerMojo.java
@@ -33,7 +33,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Writer;
-import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
