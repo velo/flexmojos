612a6dfd12295aa3a7a811789d984c7b5adc98fc
Marvin Froeder <velo.br@gmail.com>
2008-07-05 20:10:06 +0000

    IT for issue 61

---
+++ rvin-mojo/flex-mojo-IT/src/test/resources/issues/issue-0061/java/src/main/java/info/flexmojos/generator/SimpleChild.java
@@ -0,0 +1,17 @@
+package info.flexmojos.generator;
+
+import java.util.List;
+
+public class SimpleChild extends SimplePojo {
+
+	private Double childValue;
+
+	public Double getChildValue() {
+		return childValue;
+	}
+
+	public void setChildValue(Double value) {
+		this.childValue = value;
+	}
+
+}
