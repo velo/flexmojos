60539ab7dcd66d88f9fbdfbc45a4376845572c65
Marvin Froeder <velo.br@gmail.com>
2008-07-18 01:51:22 +0000

    Add some tests.

--- rvin-mojo/flex-mojo-IT/src/test/java/info/rvin/mojo/flexmojo/test/IT0013IssuesTest.java
+++ rvin-mojo/flex-mojo-IT/src/test/java/info/rvin/mojo/flexmojo/test/IT0013IssuesTest.java
@@ -1,11 +1,15 @@
 package info.rvin.mojo.flexmojo.test;
 
 import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.maven.integrationtests.AbstractMavenIntegrationTestCase;
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
+import org.codehaus.plexus.util.IOUtil;
 
 public class IT0013IssuesTest extends AbstractMavenIntegrationTestCase {
 
@@ -19,9 +23,16 @@ public class IT0013IssuesTest extends AbstractMavenIntegrationTestCase {
 	private void customIssueTester(File testDir, String groupId,
 			String artifactId, String version, String type, String goal)
 			throws Exception {
+		customIssueTester(testDir, groupId, artifactId, version, type, goal,
+				new HashMap<String, String>());
+	}
+
+	private void customIssueTester(File testDir, String groupId,
+			String artifactId, String version, String type, String goal,
+			Map<String, String> args) throws Exception {
 		Verifier verifier = new Verifier(testDir.getAbsolutePath(), true);
 		verifier.deleteArtifact(groupId, artifactId, version, type);
-		verifier.executeGoal(goal);
+		verifier.executeGoal(goal, args);
 		verifier.verifyErrorFreeLog();
 		verifier.resetStreams();
 	}
@@ -79,7 +90,7 @@ public class IT0013IssuesTest extends AbstractMavenIntegrationTestCase {
 		customIssueTester(testDir, "info.rvin.itest.issues", "issue-0017",
 				"1.0-SNAPSHOT", "swf", "asdoc:asdoc");
 
-		File asdoc = new File(testDir, "target/asdocs");
+		File asdoc = new File(testDir, "target/asdoc");
 		assertTrue("asdoc directory must exist", asdoc.isDirectory());
 	}
 
@@ -102,6 +113,15 @@ public class IT0013IssuesTest extends AbstractMavenIntegrationTestCase {
 		standardIssueTester("issue-0039");
 	}
 
+	public void testIssue43() throws Exception {
+		File testDir = ResourceExtractor.simpleExtractResources(getClass(),
+				"/issues/issue-0014");
+		Map<String, String> args = new HashMap<String, String>();
+		args.put("maven.test.failure.ignore", "true");
+		customIssueTester(testDir, "info.rvin.itest.issues", "issue-0014",
+				"1.0-SNAPSHOT", "swf", "install", args);
+	}
+
 	public void testIssue44() throws Exception {
 		File testDir = ResourceExtractor.simpleExtractResources(getClass(),
 				"/issues/issue-0044");
@@ -112,6 +132,20 @@ public class IT0013IssuesTest extends AbstractMavenIntegrationTestCase {
 		} catch (Exception e) {
 			// expected exception
 		}
+	}
+
+	public void testIssue53() throws Exception {
+		File testDir = ResourceExtractor.simpleExtractResources(getClass(),
+				"/issues/issue-0014");
+		Map<String, String> args = new HashMap<String, String>();
+		args.put("maven.test.skip", "true");
+		customIssueTester(testDir, "info.rvin.itest.issues", "issue-0014",
+				"1.0-SNAPSHOT", "swf", "install", args);
+
+		args = new HashMap<String, String>();
+		args.put("skipTests", "true");
+		customIssueTester(testDir, "info.rvin.itest.issues", "issue-0014",
+				"1.0-SNAPSHOT", "swf", "install", args);
 	}
 
 	public void testIssue61() throws Exception {
@@ -133,7 +167,42 @@ public class IT0013IssuesTest extends AbstractMavenIntegrationTestCase {
 	}
 
 	public void testIssue69() throws Exception {
+		final String[] trusts = new String[] {
+				"AppData/Roaming/Macromedia/Flash Player/#Security/FlashPlayerTrust",
+				"Application Data/Macromedia/Flash Player/#Security/FlashPlayerTrust",
+				".macromedia/Flash_Player/#Security/FlashPlayerTrust",
+				"Library/Preferences/Macromedia/Flash Player/#Security/FlashPlayerTrust" };
+
+		File userHome = new File(System.getProperty("user.home"));
+
+		File mavenCfg = null;
+		for (String folder : trusts) {
+			File fpTrustFolder = new File(userHome, folder);
+			if (fpTrustFolder.exists() && fpTrustFolder.isDirectory()) {
+				mavenCfg = new File(fpTrustFolder, "maven.cfg");
+				if (mavenCfg.exists()) {
+					mavenCfg.delete();
+				}
+				break;
+			}
+		}
+
 		standardIssueTester("issue-0069");
+
+		File testDir = ResourceExtractor.simpleExtractResources(getClass(),
+				"/issues/issue-0069");
+
+		assertTrue("Flex-mojos should generate maven.cfg: "
+				+ mavenCfg.getAbsolutePath(), mavenCfg.exists());
+
+		String cfg = IOUtil.toString(new FileReader(mavenCfg));
+
+		assertTrue("Flex-mojos should write trust localtion", cfg
+				.contains(testDir.getAbsolutePath()));
+	}
+
+	public void testIssue70() throws Exception {
+		standardIssueTester("issue-0070");
 	}
 
 }
