2c64174fe03a7721da4bd3ff5f9f22591c4a0b79
Marvin Froeder <velo.br@gmail.com>
2008-07-22 18:36:17 +0000

    Removed unused constructor.

--- rvin-mojo/asdoc-mojo/src/main/java/info/rvin/flexmojos/asdoc/Namespace.java
+++ rvin-mojo/asdoc-mojo/src/main/java/info/rvin/flexmojos/asdoc/Namespace.java
@@ -12,12 +12,6 @@ public class Namespace {
 		super();
 	}
 
-	public Namespace(String uri, File manifest) {
-		super();
-		this.uri = uri;
-		this.manifest = manifest;
-	}
-
 	public String getUri() {
 		return uri;
 	}
