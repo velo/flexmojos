a282600ed79665dede485ba2b1bc14ee5f4ffbef
Marvin Froeder <velo.br@gmail.com>
2008-07-22 18:48:38 +0000

    Integration tests for issues 63, 65 and 66

---
+++ rvin-mojo/flex-mojo-IT/src/test/resources/issues/issue-0066/java/src/main/java/info/flexmojos/generator/AnotherPojo.java
@@ -0,0 +1,25 @@
+package info.flexmojos.generator;
+
+public class AnotherPojo {
+
+	private String name;
+
+	private Double value;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+}
