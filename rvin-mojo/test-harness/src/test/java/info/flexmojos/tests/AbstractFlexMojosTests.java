38829228568a577e48383d016a26207b6778ce67
Marvin Froeder <velo.br@gmail.com>
2008-09-19 02:11:17 +0000

    Issue 91

--- rvin-mojo/test-harness/src/test/java/info/flexmojos/tests/AbstractFlexMojosTests.java
+++ rvin-mojo/test-harness/src/test/java/info/flexmojos/tests/AbstractFlexMojosTests.java
@@ -20,19 +20,24 @@ package info.flexmojos.tests;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
 
 import junit.framework.Assert;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.apache.maven.it.Verifier;
 import org.junit.BeforeClass;
 
 public class AbstractFlexMojosTests
 {
 
-    protected static File rootFolder;
+    protected static File projectsSource;
+
+    protected static File projectsWorkdir;
 
     private static Properties props;
 
@@ -40,9 +45,6 @@ public class AbstractFlexMojosTests
     public static void initFolders()
         throws IOException
     {
-        URL rootUrl = AbstractFlexMojosTests.class.getResource( "/" );
-        rootFolder = new File( rootUrl.getFile() );
-
         props = new Properties();
         ClassLoader cl = AbstractFlexMojosTests.class.getClassLoader();
         InputStream is = cl.getResourceAsStream( "baseTest.properties" );
@@ -57,6 +59,9 @@ public class AbstractFlexMojosTests
                 is.close();
             }
         }
+
+        projectsSource = new File( "projects" );
+        projectsWorkdir = new File( "target/projects" );
     }
 
     private static synchronized String getProperty( String key )
@@ -71,10 +76,10 @@ public class AbstractFlexMojosTests
         System.setProperty( "maven.home", getProperty( "fake-maven" ) );
 
         Verifier verifier = new Verifier( projectDirectory.getAbsolutePath() );
-        verifier.resetStreams();
         // verifier.getCliOptions().add( "-s" + rootFolder.getAbsolutePath() + "/settings.xml" );
-        verifier.getCliOptions().add( "-o" );
+        // verifier.getCliOptions().add( "-o" );
         verifier.getCliOptions().addAll( Arrays.asList( args ) );
+        verifier.getCliOptions().add( "-Dflex-mojos.version=" + getProperty( "version" ) );
         verifier.getVerifierProperties().put( "use.mavenRepoLocal", "true" );
         verifier.setLocalRepo( getProperty( "fake-repo" ) );
         verifier.executeGoal( goal );
@@ -82,11 +87,26 @@ public class AbstractFlexMojosTests
     }
 
     protected File getProject( String projectName )
+        throws IOException
     {
-        File projectFolder = new File( rootFolder, projectName );
+        File projectFolder = new File( projectsSource, projectName );
         Assert.assertTrue( "Project " + projectName + " folder not found.\n" + projectFolder.getAbsolutePath(),
                            projectFolder.isDirectory() );
-        return projectFolder;
+
+        File destDir = new File( projectsWorkdir, projectName );
+        FileUtils.copyDirectory( projectFolder, destDir );
+
+        // projects filtering
+        Collection<File> poms =
+            FileUtils.listFiles( destDir, new WildcardFileFilter( "pom.xml" ), DirectoryFileFilter.INSTANCE );
+        for ( File pom : poms )
+        {
+            String pomContent = FileUtils.readFileToString( pom );
+            pomContent = pomContent.replace( "%{flex-mojos.version}", getProperty( "version" ) );
+            FileUtils.writeStringToFile( pom, pomContent );
+        }
+
+        return destDir;
     }
 
 }
