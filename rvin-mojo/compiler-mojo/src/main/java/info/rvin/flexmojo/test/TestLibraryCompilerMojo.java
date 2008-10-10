01b839803bf0dbdc6345dc5c02b591a22c45684c
Marvin Froeder <velo.br@gmail.com>
2008-10-10 12:42:17 +0000

    Fix issue 113

--- rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/TestLibraryCompilerMojo.java
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/TestLibraryCompilerMojo.java
@@ -68,7 +68,7 @@ public class TestLibraryCompilerMojo
 
         outputFile = new File( build.getDirectory(), build.getFinalName() + "-test.swc" );
 
-        includeSources = MavenUtils.getTestSourcePaths( build );
+        includeSources = MavenUtils.getTestSourcePaths( project, build );
 
         super.setUp();
     }
@@ -82,7 +82,7 @@ public class TestLibraryCompilerMojo
         // add test libraries
         configuration.addLibraryPath( getDependenciesPath( "test" ) );
 
-        configuration.addSourcePath( MavenUtils.getTestSourcePaths( build ) );
+        configuration.addSourcePath( MavenUtils.getTestSourcePaths( project, build ) );
     }
 
     @Override
