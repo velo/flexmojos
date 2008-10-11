0fc5caf4ca031546ea137f80b39ca47e83fdaf32
Marvin Froeder <velo.br@gmail.com>
2008-10-11 13:30:53 +0000

    Update granite-generator to 1.1.0 Issue 121

--- rvin-mojo/generator-mojo/src/main/java/info/flexmojos/generator/Gas3Listener.java
+++ rvin-mojo/generator-mojo/src/main/java/info/flexmojos/generator/Gas3Listener.java
@@ -18,8 +18,9 @@
 package info.flexmojos.generator;
 
 import org.apache.maven.plugin.logging.Log;
-import org.granite.generator.GenerationListener;
-import org.granite.generator.as3.JavaFileGenerationUnit;
+import org.granite.generator.Input;
+import org.granite.generator.Listener;
+import org.granite.generator.Output;
 
 /**
  * Logging <code>GenerationListener</code>.
@@ -27,7 +28,7 @@ import org.granite.generator.as3.JavaFileGenerationUnit;
  * @author Juraj Burian
  */
 public class Gas3Listener
-    implements GenerationListener<JavaFileGenerationUnit>
+    implements Listener
 {
 
     private final Log log;
@@ -45,14 +46,9 @@ public class Gas3Listener
         error( message, null );
     }
 
-    public void error( String message, Exception e )
+    public void error( String message, Throwable e )
     {
         log.error( message, e );
-    }
-
-    public void generating( JavaFileGenerationUnit unit )
-    {
-        info( "  Generating: " + unit.getOutput() );
     }
 
     public void info( String message )
@@ -60,7 +56,7 @@ public class Gas3Listener
         info( message, null );
     }
 
-    public void info( String message, Exception e )
+    public void info( String message, Throwable e )
     {
         log.info( message, e );
     }
@@ -70,9 +66,19 @@ public class Gas3Listener
         warn( message, null );
     }
 
-    public void warn( String message, Exception e )
+    public void warn( String message, Throwable e )
     {
         log.warn( message, e );
+    }
+
+    public void generating(Input<?> input, Output<?> output) {
+        info( "  Generating: " + output.getDescription() );
+        
+    }
+
+    public void skipping(Input<?> input, Output<?> output) {
+        info( "  Skipping: " + output.getDescription() );
+        
     }
 
 }
