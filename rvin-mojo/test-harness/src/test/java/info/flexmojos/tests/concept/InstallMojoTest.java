71b6f99a5cf9b261122de605bf42f46f674eb569
Marvin Froeder <velo.br@gmail.com>
2008-10-18 00:00:55 +0000

    Created integration tests to install-mojo

---
+++ rvin-mojo/test-harness/src/test/java/info/flexmojos/tests/concept/InstallMojoTest.java
@@ -0,0 +1,72 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.flexmojos.tests.concept;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.apache.maven.it.Verifier;
+import org.junit.Test;
+
+public class InstallMojoTest
+    extends AbstractConceptTest
+{
+
+    @SuppressWarnings( "unchecked" )
+    @Test
+    public void installFake()
+        throws Exception
+    {
+        File testDir = getProject( "/concept/install-sdk" );
+        File sdkDir = new File( testDir, "fake-sdk" );
+
+        Verifier verifier = getVerifier( testDir );
+        verifier.setAutoclean( false );
+        verifier.getCliOptions().add( "-Dflex.sdk.folder=" + sdkDir.getAbsolutePath() );
+        verifier.getCliOptions().add( "-Dversion=1.0.0-fake" );
+        verifier.executeGoal( "info.flex-mojos:install-mojo:" + getProperty( "version" ) + ":install-sdk" );
+        verifier.verifyErrorFreeLog();
+
+        File repoDir = new File( getProperty( "fake-repo" ) );
+
+        // compiler stuff
+        File compilerPom = new File( repoDir, "com/adobe/flex/compiler/1.0.0-fake/compiler-1.0.0-fake.pom" );
+        Assert.assertTrue( compilerPom.exists() );
+        File compilerLibrary =
+            new File( repoDir, "com/adobe/flex/compiler/compiler-library/1.0.0-fake/compiler-library-1.0.0-fake.jar" );
+        Assert.assertTrue( compilerLibrary.exists() );
+
+        // framework stuff
+        File flexFrameworkPom =
+            new File( repoDir, "com/adobe/flex/framework/flex-framework/1.0.0-fake/flex-framework-1.0.0-fake.pom" );
+        Assert.assertTrue( flexFrameworkPom.exists() );
+        File airFrameworkPom =
+            new File( repoDir, "com/adobe/flex/framework/air-framework/1.0.0-fake/air-framework-1.0.0-fake.pom" );
+        Assert.assertTrue( airFrameworkPom.exists() );
+        File flexLibrary =
+            new File( repoDir, "com/adobe/flex/framework/flex-library/1.0.0-fake/flex-library-1.0.0-fake.swc" );
+        Assert.assertTrue( flexLibrary.exists() );
+        File flexLibraryBeaconLocale =
+            new File( repoDir, "com/adobe/flex/framework/flex-library/1.0.0-fake/flex-library-1.0.0-fake.rb.swc" );
+        Assert.assertTrue( flexLibraryBeaconLocale.exists() );
+        File flexLibraryEnUsLocale =
+            new File( repoDir, "com/adobe/flex/framework/flex-library/1.0.0-fake/flex-library-1.0.0-fake-en_US.rb.swc" );
+        Assert.assertTrue( flexLibraryEnUsLocale.exists() );
+    }
+}
