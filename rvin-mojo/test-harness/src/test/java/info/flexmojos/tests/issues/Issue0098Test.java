93ee95e9db2b88f24a926a1ff7f841caa4830e57
Marvin Froeder <velo.br@gmail.com>
2008-10-22 16:12:42 +0000

    issue 98 IT

--- rvin-mojo/test-harness/src/test/java/info/flexmojos/tests/issues/Issue0098Test.java
+++ rvin-mojo/test-harness/src/test/java/info/flexmojos/tests/issues/Issue0098Test.java
@@ -1,16 +1,25 @@
 package info.flexmojos.tests.issues;
 
+import java.io.File;
+
+import org.apache.maven.it.Verifier;
 import org.junit.Test;
 
 public class Issue0098Test
     extends AbstractIssueTest
 {
 
+    @SuppressWarnings( "unchecked" )
     @Test
     public void issue98()
         throws Exception
     {
-        testIssue( "issue-0098" );
+        File testDir = getProject( "/issues/issue-0098" );
+
+        Verifier verifier = getVerifier( testDir );
+        verifier.getCliOptions().add( "-DinjectedNumber=3" );
+        verifier.executeGoal( "install" );
+        verifier.verifyErrorFreeLog();
     }
 
 }
