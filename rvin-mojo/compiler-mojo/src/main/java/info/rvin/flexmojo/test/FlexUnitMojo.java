73cbf0ec798bc50f81ff8d167050844a1c72d7d1
Logan Allred <redbugz@gmail.com>
2008-11-19 16:07:57 +0000

    fix for issue#38, flexUnitCommand is now passed to the launcher

--- rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/FlexUnitMojo.java
+++ rvin-mojo/compiler-mojo/src/main/java/info/rvin/flexmojo/test/FlexUnitMojo.java
@@ -445,7 +445,7 @@ public class FlexUnitMojo
         getLog().info( "flexunit setup args: " + flexUnitCommand );
 
         // Start the browser and run the FlexUnit tests.
-        final FlexUnitLauncher browser = new FlexUnitLauncher();
+        final FlexUnitLauncher browser = new FlexUnitLauncher(flexUnitCommand);
         try
         {
             browser.runTests( swf );
