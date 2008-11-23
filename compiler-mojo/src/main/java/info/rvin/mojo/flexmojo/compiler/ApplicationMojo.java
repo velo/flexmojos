0a51ba6b70731cc2e0b237b007678ddb1867ec8a
Marvin Froeder <velo.br@gmail.com>
2008-11-23 17:59:51 +0000

    Fixed issue 76

--- compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/ApplicationMojo.java
+++ compiler-mojo/src/main/java/info/rvin/mojo/flexmojo/compiler/ApplicationMojo.java
@@ -46,6 +46,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -431,6 +432,12 @@ public class ApplicationMojo
     protected String getDefaultLocale()
     {
         return this.defaultLocale;
+    }
+
+    @Override
+    protected List<File> getResourceBundlePath()
+    {
+        return Collections.singletonList( new File( resourceBundlePath ) );
     }
 
 }
