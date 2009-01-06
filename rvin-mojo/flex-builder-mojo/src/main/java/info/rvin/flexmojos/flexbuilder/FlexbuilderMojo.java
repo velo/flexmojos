8cddea6e30cd1c8e4d876de48af5e0fb6f8255bd
Marvin Froeder <velo.br@gmail.com>
2009-01-06 17:10:17 +0000

    Working on flexbuilder:flexbuilder

--- rvin-mojo/flex-builder-mojo/src/main/java/info/rvin/flexmojos/flexbuilder/FlexbuilderMojo.java
+++ rvin-mojo/flex-builder-mojo/src/main/java/info/rvin/flexmojos/flexbuilder/FlexbuilderMojo.java
@@ -18,105 +18,340 @@
  */
 package info.rvin.flexmojos.flexbuilder;
 
+import static info.rvin.flexmojos.utilities.MavenUtils.resolveArtifact;
+import info.flexmojos.utilities.PathUtil;
 import info.rvin.flexmojos.utilities.CompileConfigurationLoader;
 import info.rvin.flexmojos.utilities.MavenUtils;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
-import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
 import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.eclipse.EclipsePlugin;
 import org.apache.maven.plugin.ide.IdeDependency;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
+import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.velocity.VelocityComponent;
 
 /**
  * @extendsPlugin eclipse
  * @extendsGoal eclipse
- * 
  * @goal flexbuilder
  * @phase package
  * @requiresDependencyResolution
  */
-public class FlexbuilderMojo extends EclipsePlugin {
+public class FlexbuilderMojo
+    extends EclipsePlugin
+{
 
-	/**
-	 * @component
-	 */
-	private VelocityComponent velocityComponent;
+    private static final String SWC = "swc";
 
-	@Override
-	public void writeConfiguration(IdeDependency[] deps)
-			throws MojoExecutionException {
-		super.writeConfiguration(deps);
+    private static final String SWF = "swf";
 
-		try {
-			writeAsProperties();
-		} catch (Exception e) {
-			throw new MojoExecutionException(
-					"Error writting .actionScriptProperties", e);
-		}
+    private static final String RB_SWC = "rb.swc";
 
-	}
+    /**
+     * @component
+     */
+    private VelocityComponent velocityComponent;
 
-	/**
-	 * @parameter expression="${project.remoteArtifactRepositories}"
-	 */
-	@SuppressWarnings("unchecked")
-	protected List remoteRepositories;
-	/**
-	 * @component
-	 */
-	protected ArtifactResolver resolver;
+    @SuppressWarnings( "unchecked" )
+    @Override
+    protected void fillDefaultNatures( String packaging )
+    {
+        super.fillDefaultNatures( packaging );
 
-	@SuppressWarnings("unchecked")
-	protected Set<Artifact> getDependencyArtifacts()
-			throws MojoExecutionException {
-		ArtifactResolutionResult arr;
-		try {
-			arr = resolver
-					.resolveTransitively(project.getDependencyArtifacts(),
-							project.getArtifact(), remoteRepositories,
-							localRepository, artifactMetadataSource);
-		} catch (ArtifactResolutionException e) {
-			throw new MojoExecutionException(e.getMessage(), e);
-		} catch (ArtifactNotFoundException e) {
-			throw new MojoExecutionException(e.getMessage(), e);
-		}
-		Set<Artifact> result = arr.getArtifacts();
-		return result;
-	}
+        if ( SWF.equals( packaging ) )
+        {
+            getProjectnatures().add( "com.adobe.flexbuilder.project.flexnature" );
+            getProjectnatures().add( "com.adobe.flexbuilder.project.actionscriptnature" );
+        }
 
-	private void writeAsProperties() throws Exception {
-		VelocityContext context = new VelocityContext();
-		context.put("dependencies", getDependencyArtifacts());
-		context.put("mainApplication", MavenUtils.resolveSourceFile(project,
-				CompileConfigurationLoader.getCompilerPluginSetting(project,
-						"sourceFile")));
+        if ( SWC.equals( packaging ) )
+        {
+            getProjectnatures().add( "com.adobe.flexbuilder.project.flexlibnature" );
+            getProjectnatures().add( "com.adobe.flexbuilder.project.actionscriptnature" );
+        }
+    }
 
-		Template template = velocityComponent.getEngine().getTemplate(
-				"/actionScriptProperties.vm");
+    @SuppressWarnings( "unchecked" )
+    @Override
+    protected void fillDefaultBuilders( String packaging )
+    {
+        super.fillDefaultBuilders( packaging );
 
-		Writer writer = null;
-		try {
-			writer = new FileWriter(new File(project.getBasedir(),
-					".actionScriptProperties"));
-			template.merge(context, writer);
-		} finally {
-			if (writer != null) {
-				writer.flush();
-				writer.close();
-			}
-		}
-	}
+        if ( SWF.equals( packaging ) || SWC.equals( packaging ) )
+        {
+            getBuildcommands().add( "com.adobe.flexbuilder.project.flexbuilder" );
+        }
+    }
+
+    @Override
+    public void writeConfiguration( IdeDependency[] deps )
+        throws MojoExecutionException
+    {
+        super.writeConfiguration( deps );
+
+        String packaging = project.getPackaging();
+        writeAsProperties( packaging );
+
+        if ( SWF.equals( packaging ) )
+        {
+            writeFlexProperties();
+        }
+        else if ( SWC.equals( packaging ) )
+        {
+            writeFlexLibProperties();
+        }
+        else
+        {
+            throw new MojoExecutionException( "Unexpected packaging " + packaging );
+        }
+
+    }
+
+    private void writeFlexLibProperties()
+        throws MojoExecutionException
+    {
+        VelocityContext context = new VelocityContext();
+        // TODO
+        // context.put( "flexClasses", classes );
+        // context.put( "includeFiles", files );
+
+        runVelocity( "/flexLibProperties.vm", ".flexLibProperties", context );
+    }
+
+    private void writeFlexProperties()
+        throws MojoExecutionException
+    {
+        VelocityContext context = new VelocityContext();
+
+        runVelocity( "/flexProperties.vm", ".flexProperties", context );
+    }
+
+    /**
+     * @parameter expression="${project.remoteArtifactRepositories}"
+     */
+    @SuppressWarnings( "unchecked" )
+    protected List remoteRepositories;
+
+    /**
+     * @component
+     */
+    protected ArtifactResolver resolver;
+
+    @SuppressWarnings( "unchecked" )
+    protected Set<Artifact> getDependencyArtifacts()
+        throws MojoExecutionException
+    {
+        ArtifactResolutionResult arr;
+        try
+        {
+            arr =
+                resolver.resolveTransitively( project.getDependencyArtifacts(), project.getArtifact(),
+                                              remoteRepositories, localRepository, artifactMetadataSource );
+        }
+        catch ( AbstractArtifactResolutionException e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+        List<Artifact> dependencies = new ArrayList<Artifact>( arr.getArtifacts() );
+        List<Artifact> extraRbs = resolveResourceBundles( dependencies );
+
+        Set<Artifact> result = new HashSet<Artifact>();
+        result.addAll( dependencies );
+        result.addAll( extraRbs );
+        return result;
+    }
+
+    private List<Artifact> resolveResourceBundles( List<Artifact> dependencies )
+        throws MojoExecutionException
+    {
+        List<Artifact> extraRbs = new ArrayList<Artifact>();
+        String[] locales = getLocales();
+
+        for ( Iterator<Artifact> it = dependencies.iterator(); it.hasNext(); )
+        {
+            Artifact dependency = it.next();
+            if ( "playerglobal".equals( dependency.getArtifactId() ) || "airglobal".equals( dependency.getArtifactId() ) )
+            {
+                it.remove();
+            }
+            else if ( SWC.equals( dependency.getType() ) )
+            {
+                continue;
+            }
+            else if ( RB_SWC.equals( dependency.getType() ) )
+            {
+                for ( String locale : locales )
+                {
+                    Artifact resolvedResourceBundle =
+                        artifactFactory.createArtifactWithClassifier( dependency.getGroupId(),
+                                                                      dependency.getArtifactId(),
+                                                                      dependency.getVersion(), dependency.getType(),
+                                                                      locale );
+
+                    resolveArtifact( resolvedResourceBundle, resolver, localRepository, remoteRepositories );
+                    extraRbs.add( resolvedResourceBundle );
+                }
+                it.remove();
+            }
+            else
+            {
+                it.remove();
+            }
+        }
+        return extraRbs;
+    }
+
+    private String[] getLocales()
+    {
+        String[] locales = CompileConfigurationLoader.getCompilerPluginSettings( project, "locales" );
+        String[] runtimeLocales = CompileConfigurationLoader.getCompilerPluginSettings( project, "runtimeLocales" );
+        String[] compiledLocales = CompileConfigurationLoader.getCompilerPluginSettings( project, "compiledLocales" );
+        String defaultLocale = CompileConfigurationLoader.getCompilerPluginSetting( project, "defaultLocale" );
+
+        locales = (String[]) ArrayUtils.addAll( locales, runtimeLocales );
+        locales = (String[]) ArrayUtils.addAll( locales, compiledLocales );
+        if ( defaultLocale != null )
+        {
+            locales = (String[]) ArrayUtils.add( locales, defaultLocale );
+        }
+        return locales;
+    }
+
+    private void writeAsProperties( String packaging )
+        throws MojoExecutionException
+    {
+        VelocityContext context = new VelocityContext();
+        context.put( "dependencies", getDependencyArtifacts() );
+        context.put( "locales", getPlainLocales() );
+        context.put( "mainSources", getMainSources() );
+        if ( SWF.equals( packaging ) )
+        {
+            File sourceFile =
+                MavenUtils.resolveSourceFile( project,
+                                              CompileConfigurationLoader.getCompilerPluginSetting( project,
+                                                                                                   "sourceFile" ) );
+            context.put( "mainApplication", sourceFile.getName() );
+        }
+        else if ( SWC.equals( packaging ) )
+        {
+            context.put( "mainApplication", project.getArtifactId() + ".as" );
+            context.put( "includes", "-include-sources " + getPlainSources() );
+        }
+        context.put( "sources", getRelativeSources() );
+        runVelocity( "/actionScriptProperties.vm", ".actionScriptProperties", context );
+    }
+
+    private String getMainSources()
+    {
+        String mainSources =
+            PathUtil.getRelativePath( project.getBasedir(), new File( project.getBuild().getSourceDirectory() ) );
+        return mainSources;
+    }
+
+    private String getPlainSources()
+    {
+        String[] sources = CompileConfigurationLoader.getCompilerPluginSettings( project, "includeSources" );
+        if ( sources == null )
+        {
+            sources = new String[] { project.getBuild().getSourceDirectory() };
+        }
+        return plain( sources );
+    }
+
+    private List<String> getRelativeSources()
+    {
+        List<String> sources = new ArrayList<String>();
+        List<String> sourceRoots = getSourceRoots();
+        for ( String sourceRoot : sourceRoots )
+        {
+            File source = new File( sourceRoot );
+            if ( source.exists() )
+            {
+                String relative = PathUtil.getRelativePath( project.getBasedir(), source );
+                sources.add( relative );
+            }
+        }
+        return sources;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    private List<String> getSourceRoots()
+    {
+        List<String> sources = new ArrayList<String>();
+        sources.addAll( project.getCompileSourceRoots() );
+        sources.addAll( project.getTestCompileSourceRoots() );
+        for ( Resource resource : (List<Resource>) project.getBuild().getResources() )
+        {
+            sources.add( resource.getDirectory() );
+        }
+        for ( Resource resource : (List<Resource>) project.getBuild().getTestResources() )
+        {
+            sources.add( resource.getDirectory() );
+        }
+        return sources;
+    }
+
+    private String getPlainLocales()
+    {
+        String[] locales = getLocales();
+        String buf = plain( locales );
+        return buf;
+    }
+
+    private String plain( String[] locales )
+    {
+        StringBuilder buf = new StringBuilder();
+        for ( String locale : locales )
+        {
+            if ( buf.length() != 0 )
+            {
+                buf.append( ' ' );
+            }
+            buf.append( locale );
+        }
+        return buf.toString();
+    }
+
+    private void runVelocity( String templateName, String fileName, VelocityContext context )
+        throws MojoExecutionException
+    {
+
+        Writer writer = null;
+        try
+        {
+            Template template = velocityComponent.getEngine().getTemplate( templateName );
+            writer = new FileWriter( new File( project.getBasedir(), fileName ) );
+            template.merge( context, writer );
+            writer.flush();
+        }
+        catch ( Exception e )
+        {
+            throw new MojoExecutionException( "Error writting " + fileName, e );
+        }
+        finally
+        {
+            if ( writer != null )
+            {
+                IOUtil.close( writer );
+            }
+        }
+
+    }
 
 }
