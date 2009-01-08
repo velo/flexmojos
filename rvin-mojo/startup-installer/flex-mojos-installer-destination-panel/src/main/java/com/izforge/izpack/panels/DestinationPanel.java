1e6040b381c318f139eb954e57c70775e228fa1b
Marvin Froeder <velo.br@gmail.com>
2009-01-08 10:27:38 +0000

    Update license reader

--- rvin-mojo/startup-installer/flex-mojos-installer-destination-panel/src/main/java/com/izforge/izpack/panels/DestinationPanel.java
+++ rvin-mojo/startup-installer/flex-mojos-installer-destination-panel/src/main/java/com/izforge/izpack/panels/DestinationPanel.java
@@ -1,24 +1,21 @@
-/*
- * IzPack - Copyright 2001-2008 Julien Ponge, All Rights Reserved.
+/**
+ * Flex-mojos is a set of maven plugins to allow maven users to compile, optimize, test and ... Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008 Marvin Herman Froeder
  *
- * http://izpack.org/
- * http://izpack.codehaus.org/
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Copyright 2002 Jan Blok
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package com.izforge.izpack.panels;
 
 import java.awt.LayoutManager2;
