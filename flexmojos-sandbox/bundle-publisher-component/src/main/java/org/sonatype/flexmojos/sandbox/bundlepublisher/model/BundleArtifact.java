1cdaf9f5ada56fac06f68da3fb9286409f43bbe6
Marvin Froeder <velo.br@gmail.com>
2009-01-21 14:24:16 +0000

    OPEN - issue FLEXCM-4: Publishing the contents of the Flex SDK to a Maven repository https://issues.sonatype.org/browse/FLEXCM-4

--- flexmojos-sandbox/bundle-publisher-component/src/main/java/org/sonatype/flexmojos/sandbox/bundlepublisher/model/BundleArtifact.java
+++ flexmojos-sandbox/bundle-publisher-component/src/main/java/org/sonatype/flexmojos/sandbox/bundlepublisher/model/BundleArtifact.java
@@ -7,21 +7,31 @@ import java.util.List;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.sonatype.flexmojos.sandbox.bundlepublisher.util.Xpp3Util;
 
-public class Artifact
+public class BundleArtifact
 {
 
     private Xpp3Dom dom;
 
     private List<ArtifactDependency> dependencies;
 
-    public Artifact( Xpp3Dom dom )
+    public BundleArtifact( Xpp3Dom dom )
     {
         this.dom = dom;
+    }
+
+    public String getGroupId()
+    {
+        return Xpp3Util.getValue( dom, "groupId" );
     }
 
     public String getArtifactId()
     {
         return Xpp3Util.getValue( dom, "artifactId" );
+    }
+
+    public String getVersion()
+    {
+        return Xpp3Util.getValue( dom, "version" );
     }
 
     public String getLocation()
@@ -32,6 +42,11 @@ public class Artifact
     public String getType()
     {
         return Xpp3Util.getValue( dom, "type" );
+    }
+
+    public String getClassifier()
+    {
+        return Xpp3Util.getValue( dom, "classifier" );
     }
 
     public List<ArtifactDependency> getDependencies()
