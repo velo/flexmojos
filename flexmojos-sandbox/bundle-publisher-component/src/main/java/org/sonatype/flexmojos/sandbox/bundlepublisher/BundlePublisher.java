6104de076b94afc4dce89505e26efa4ed00b18ed
Marvin Froeder <velo.br@gmail.com>
2009-01-21 14:24:16 +0000

    OPEN - issue FLEXCM-4: Publishing the contents of the Flex SDK to a Maven repository https://issues.sonatype.org/browse/FLEXCM-4

--- flexmojos-sandbox/bundle-publisher-component/src/main/java/org/sonatype/flexmojos/sandbox/bundlepublisher/BundlePublisher.java
+++ flexmojos-sandbox/bundle-publisher-component/src/main/java/org/sonatype/flexmojos/sandbox/bundlepublisher/BundlePublisher.java
@@ -20,15 +20,18 @@ package org.sonatype.flexmojos.sandbox.bundlepublisher;
 import java.io.File;
 import java.io.InputStream;
 
-import org.apache.maven.mercury.repository.api.Repository;
-import org.apache.maven.mercury.repository.api.RepositoryException;
+import org.apache.maven.artifact.repository.ArtifactRepository;
 
 public interface BundlePublisher
 {
 
     String ROLE = BundlePublisher.class.getName();
 
-    void publish( File sourceFile, InputStream bundleDescriptor, Repository repository )
-        throws PublishingException, RepositoryException;
+    void install( File sourceFile, InputStream bundleDescriptor, ArtifactRepository localRepository )
+        throws PublishingException;
+
+    void deploy( File sourceFile, InputStream bundleDescriptor, ArtifactRepository deploymentRepository,
+                 ArtifactRepository localRepository )
+        throws PublishingException;
 
 }
