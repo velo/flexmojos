6551522ad3d279b4a879a98d367aee53ff06a220
Marvin Froeder <velo.br@gmail.com>
2009-01-21 19:29:03 +0000

    Fixed issue 158

--- flex-maven-plugin/src/main/java/org/sonatype/flexmojos/htmlwrapper/HtmlWrapperMojo.java
+++ flex-maven-plugin/src/main/java/org/sonatype/flexmojos/htmlwrapper/HtmlWrapperMojo.java
@@ -17,7 +17,6 @@
  */
 package org.sonatype.flexmojos.htmlwrapper;
 
-
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
@@ -102,7 +101,7 @@ public class HtmlWrapperMojo
      * <p>
      * This mojo will look for <tt>index.template.html</tt> for replace parameters
      * 
-     * @parameter default-value="embed:client-side-detection-with-history"
+     * @parameter default-value="embed:express-installation-with-history"
      */
     private String templateURI;
 
