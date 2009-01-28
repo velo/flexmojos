c3565e037b0f34c81f1ec757cd4bcd207c61a1ff
Marvin Froeder <velo.br@gmail.com>
2009-01-28 18:53:34 +0000

    OPEN - issue FLEXCM-61: Moved bundle publisher outside flex-mojos https://issues.sonatype.org/browse/FLEXCM-61

--- flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/AbstractFlexMojosTests.java
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/AbstractFlexMojosTests.java
@@ -173,10 +173,6 @@ public class AbstractFlexMojosTests
                                     projectFolder.isDirectory() );
 
             File destDir = new File( projectsWorkdir, projectName );
-            if ( destDir.exists() )
-            {
-                FileUtils.forceDelete( destDir );
-            }
 
             FileUtils.copyDirectory( projectFolder, destDir, HiddenFileFilter.VISIBLE );
 
