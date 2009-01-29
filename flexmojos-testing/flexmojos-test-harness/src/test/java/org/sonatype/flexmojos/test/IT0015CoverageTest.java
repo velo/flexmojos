954f75097b55890195106160dbd463df8d48f7ab
Marvin Froeder <velo.br@gmail.com>
2009-01-29 23:52:44 +0000

    Improove file resolver test

--- flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/test/IT0015CoverageTest.java
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/test/IT0015CoverageTest.java
@@ -12,26 +12,12 @@ import static org.testng.AssertJUnit.assertTrue;
 
 import java.io.File;
 
-import org.sonatype.flexmojos.tests.AbstractFlexMojosTests;
+import org.sonatype.flexmojos.tests.coverage.AbstractCoverageTest;
 import org.testng.annotations.Test;
 
 public class IT0015CoverageTest
-    extends AbstractFlexMojosTests
+    extends AbstractCoverageTest
 {
-
-    public void standardConceptTester( String coverageName )
-        throws Exception
-    {
-        File testDir = getProject( "/coverage/" + coverageName );
-        test( testDir, "install" );
-    }
-
-    @Test
-    public void testSourceFileResolver()
-        throws Exception
-    {
-        standardConceptTester( "source-file-resolver" );
-    }
 
     @Test
     public void testAsdocInclusionExclusion()
@@ -80,7 +66,7 @@ public class IT0015CoverageTest
     public void testCompilationOptions()
         throws Exception
     {
-        standardConceptTester( "compilation-options" );
+        standardCoverageTester( "compilation-options" );
     }
 
 }
