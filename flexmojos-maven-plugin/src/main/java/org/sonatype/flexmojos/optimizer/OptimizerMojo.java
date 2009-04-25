8584dbc0cdb95253604cff61b2b2ac9041dad12c
Marvin Froeder <velo.br@gmail.com>
2009-04-25 02:27:26 +0000

    OPEN - issue FLEXMOJOS-82: Add support to SWF optimization https://issues.sonatype.org/browse/FLEXMOJOS-82

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/optimizer/OptimizerMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/optimizer/OptimizerMojo.java
@@ -17,19 +17,17 @@
  */
 package org.sonatype.flexmojos.optimizer;
 
-import eu.cedarsoft.utils.ZipExtractor;
-import flex2.compiler.swc.Digest;
-import flex2.compiler.swc.Swc;
-import flex2.compiler.swc.SwcCache;
-import flex2.compiler.swc.SwcGroup;
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 import org.apache.maven.model.Build;
 import org.apache.maven.plugin.AbstractMojo;
@@ -37,12 +35,20 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
 import org.sonatype.flexmojos.utilities.MavenUtils;
+
+import flex2.compiler.swc.Digest;
+import flex2.compiler.swc.Swc;
+import flex2.compiler.swc.SwcCache;
+import flex2.compiler.swc.SwcGroup;
 
 /**
  * Goal which run post-link SWF optimization on swc files. This goal is used to produce RSL files.
  * 
  * @author Marvin Herman Froeder (velo.br@gmail.com)
+ * @author Marla Bonar (Marla_Bonar@Intuit.com) - added ability to optimize a swf
  * @since 2.0
  * @goal optimize
  * @phase package
@@ -79,32 +85,170 @@ public class OptimizerMojo
                        "flexmojos " + MavenUtils.getFlexMojosVersion()
                            + " - GNU GPL License (NO WARRANTY) - See COPYRIGHT file" );
 
-        if ( !"swc".equals( project.getPackaging() ) )
+        String packaging = project.getPackaging();
+        getLog().debug( "project.getPackaging = " + packaging );
+
+        if ( !"swc".equals( packaging ) && !"swf".equals( packaging ) )
         {
-            getLog().warn( "Optimizer mojo can only be used on SWC projects." );
+            getLog().warn( "Optimizer mojo can only be used on SWC or SWF projects." );
             return;
         }
 
-        File library = project.getArtifact().getFile();
-        if ( library == null || !library.exists() )
+        File originalFile = project.getArtifact().getFile();
+
+        if ( originalFile == null || !originalFile.exists() )
         {
             throw new MojoExecutionException( "Library file not found." );
         }
-        File librarySWF = extractLibrarySWF( library );
-        File optimizedSWF = optimizeLibrarySWF( librarySWF );
-        Digest digest = computeDigest( optimizedSWF );
-        updateDigest( digest, library );
 
-        projectHelper.attachArtifact( project, "swf", optimizedSWF );
+        getLog().debug( "attempting to optimize: " + originalFile.getName() );
+        InputStream inputSWF = null;
+        ZipFile zipFile = null;
+        OutputStream outputSWF = null;
+        try
+        {
+            if ( "swf".equals( packaging ) )
+            {
+                backupOriginalSWF( originalFile );
+
+                inputSWF = new FileInputStream( originalFile );
+                zipFile = null;
+            }
+            else
+            {
+                zipFile = newZipFile( originalFile );
+                inputSWF = readLibrarySwf( originalFile, zipFile );
+            }
+
+            File optimizedSWFFile;
+            if ( "swf".equals( packaging ) )
+            {
+                originalFile.delete();
+                optimizedSWFFile = originalFile;
+            }
+            else
+            {
+                optimizedSWFFile = new File( build.getDirectory(), build.getFinalName() + ".swf" );
+            }
+            outputSWF = new FileOutputStream( optimizedSWFFile );
+
+            getLog().info( "Original file is: " + originalFile.length() / 1024 + " k bytes" );
+
+            optimize( inputSWF, outputSWF );
+
+            getLog().info( "optimized swf is: " + optimizedSWFFile.length() / 1024 + " k bytes" );
+
+            if ( "swc".equals( packaging ) )
+            {
+                getLog().debug( "updating digest " );
+                updateDigest( optimizedSWFFile, originalFile );
+
+                getLog().debug( "attaching Artifact " );
+                projectHelper.attachArtifact( project, "swf", optimizedSWFFile );
+            }
+        }
+        catch ( FileNotFoundException e )
+        {
+            // don't expect that
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+        finally
+        {
+            IOUtil.close( inputSWF );
+            IOUtil.close( outputSWF );
+            if ( zipFile != null )
+            {
+                try
+                {
+                    zipFile.close();
+                }
+                catch ( IOException e )
+                {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    private void optimize( InputStream inputSWF, OutputStream outputSWF )
+        throws MojoExecutionException
+    {
+        try
+        {
+            // optimize
+            flex2.tools.API.optimize( inputSWF, outputSWF );
+            outputSWF.flush();
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "An error happen while trying to optimize.", e );
+        }
+    }
+
+    private InputStream readLibrarySwf( File originalFile, ZipFile zipFile )
+        throws MojoFailureException, MojoExecutionException
+    {
+        InputStream inputSWF;
+        ZipEntry entry = zipFile.getEntry( "library.swf" );
+        if ( entry == null )
+        {
+            throw new MojoFailureException( "Invalid SWC file. Library.swf not found. " + originalFile );
+        }
+        try
+        {
+            inputSWF = zipFile.getInputStream( entry );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Unable to read library.swf entry!", e );
+        }
+        return inputSWF;
+    }
+
+    private void backupOriginalSWF( File originalFile )
+        throws MojoExecutionException
+    {
+        File bkpOriginalFile = new File( build.getOutputDirectory(), "orig-library.swf" );
+        try
+        {
+            FileUtils.copyFile( originalFile, bkpOriginalFile );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Unable to backup SWF file.", e );
+        }
+
+        getLog().debug( "attaching original swf" );
+        projectHelper.attachArtifact( project, "swf", "original", bkpOriginalFile );
+    }
+
+    private ZipFile newZipFile( File originalFile )
+        throws MojoExecutionException
+    {
+        ZipFile zipFile = null;
+        try
+        {
+            zipFile = new ZipFile( originalFile );
+        }
+        catch ( ZipException e )
+        {
+            throw new MojoExecutionException( "Invalid SWC file, is not a valid ZIP file.", e );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Unable to read SWC file. " + originalFile, e );
+        }
+        return zipFile;
     }
 
     @SuppressWarnings( "unchecked" )
-    private void updateDigest( Digest digest, File library )
+    private void updateDigest( File optimizedSWF, File originalFile )
         throws MojoExecutionException
     {
-        getLog().info( "Updating digest on " + project.getArtifact().getFile().getName() );
+        Digest digest = computeDigest( optimizedSWF );
+        getLog().debug( "Updating digest on " + originalFile );
         SwcCache swcCache = new SwcCache();
-        String[] paths = { library.getAbsolutePath() };
+        String[] paths = { originalFile.getAbsolutePath() };
         SwcGroup group = swcCache.getSwcGroup( paths );
         Collection<Swc> swcs = group.getSwcs().values();
         Swc swc = swcs.iterator().next();
@@ -119,65 +263,36 @@ public class OptimizerMojo
         {
             throw new MojoExecutionException( "Unable to update digest information.", e );
         }
+        finally
+        {
+            swc.close();
+        }
+
     }
 
-    private Digest computeDigest( File swf )
+    private Digest computeDigest( File optimizedSwf )
         throws MojoExecutionException
     {
-        getLog().info( "Computing optimized swf digest" );
+        getLog().debug( "Computing optimized swf digest" );
+        InputStream input = null;
+        byte[] bytes;
         try
         {
-            FileInputStream input = new FileInputStream( swf );
-            byte[] bytes = new byte[(int) swf.length()];
-            input.read( bytes );
-            Digest d = new Digest();
-            getLog().debug( d.computeDigest( bytes ) );
-            return d;
+            input = new FileInputStream( optimizedSwf );
+            bytes = IOUtil.toByteArray( input );
         }
         catch ( IOException e )
         {
-            throw new MojoExecutionException( "", e );
+            throw new MojoExecutionException( "Error reading optimized SWF", e );
         }
-    }
-
-    private File optimizeLibrarySWF( File librarySWF )
-        throws MojoExecutionException
-    {
-        getLog().info( "Optimizing library.swc" );
-        File optimizedSWF = new File( build.getDirectory(), build.getFinalName() + ".swf" );
-
-        try
+        finally
         {
-            InputStream input = new FileInputStream( librarySWF );
-            OutputStream output = new FileOutputStream( optimizedSWF );
-            // optimize
-            flex2.tools.API.optimize( input, output );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "An error happen while trying to optimize SWC.", e );
+            IOUtil.close( input );
         }
 
-        return optimizedSWF;
-    }
-
-    private File extractLibrarySWF( File library )
-        throws MojoExecutionException
-    {
-        getLog().info( "Extracting library.swf" );
-        File outputDir = new File( build.getOutputDirectory() );
-        try
-        {
-            ZipExtractor ze = new ZipExtractor( library );
-            ze.extract( outputDir );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "An error happens when trying to extract library.swf from your SWC.", e );
-        }
-
-        File libraryFile = new File( outputDir, "library.swf" );
-        return libraryFile;
+        Digest d = new Digest();
+        getLog().debug( d.computeDigest( bytes ) );
+        return d;
     }
 
 }
