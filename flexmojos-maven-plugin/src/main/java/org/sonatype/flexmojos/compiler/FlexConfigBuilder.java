8cb4de45c6e07af932c6f90e4be2e30563dba549
Marvin Froeder <velo.br@gmail.com>
2009-05-02 18:50:48 +0000

    Fixing warnings

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/compiler/FlexConfigBuilder.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/compiler/FlexConfigBuilder.java
@@ -17,197 +17,212 @@
  */
 package org.sonatype.flexmojos.compiler;
 
-import org.jdom.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.jdom.Comment;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
 import org.jdom.Namespace;
 import org.jdom.input.SAXBuilder;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.commons.io.IOUtils;
-
-import java.io.*;
-import java.util.Iterator;
-import java.util.List;
 
 import flex2.tools.oem.Builder;
 import flex2.tools.oem.Report;
 
 public class FlexConfigBuilder
 {
-	private Namespace namespace;
-	private Document document;
-	private Element rootElement;
+    private Namespace namespace;
 
-	public FlexConfigBuilder( String config ) throws IOException, JDOMException
-	{
-		parse( config );
-	}
+    private Document document;
 
-	public FlexConfigBuilder( Builder builder ) throws IOException, JDOMException
-	{
-		Report report = builder.getReport();
-		StringWriter stringWriter = new StringWriter();
-		report.writeConfigurationReport( stringWriter );
-		parse( stringWriter.toString() );
-	}
+    private Element rootElement;
 
-	public void parse( String config ) throws IOException, JDOMException
-	{
-		SAXBuilder parser = new SAXBuilder();
-		document = parser.build( new StringReader( config ) );
+    public FlexConfigBuilder( String config )
+        throws IOException, JDOMException
+    {
+        parse( config );
+    }
 
-		// https://bugs.adobe.com/jira/browse/SDK-19122
-		rootElement = document.getRootElement();
-		if ( rootElement.getNamespace() == Namespace.NO_NAMESPACE )
-		{
-			namespace = Namespace.getNamespace( "http://www.adobe.com/2006/flex-config" );
-			rootElement.setNamespace( namespace );
-		}
+    public FlexConfigBuilder( Builder builder )
+        throws IOException, JDOMException
+    {
+        Report report = builder.getReport();
+        StringWriter stringWriter = new StringWriter();
+        report.writeConfigurationReport( stringWriter );
+        parse( stringWriter.toString() );
+    }
 
-		removeCommentsAndSetNamespace( document.getContent() );
+    public void parse( String config )
+        throws IOException, JDOMException
+    {
+        SAXBuilder parser = new SAXBuilder();
+        document = parser.build( new StringReader( config ) );
 
-		if ( namespace == null )
-		{
-			namespace = rootElement.getNamespace();
-		}
-	}
-
-	public void write( Writer writer ) throws IOException
-	{
-		XMLOutputter outputter = new XMLOutputter();
-		Format format = Format.getPrettyFormat();
-		format.setIndent( "\t" );
-		outputter.setFormat( format );
-
-		outputter.output( document, writer );
-		writer.flush();
-	}
-
-	public void write( File file ) throws IOException
-	{
-		FileWriter writer = null;
-		try
+        // https://bugs.adobe.com/jira/browse/SDK-19122
+        rootElement = document.getRootElement();
+        if ( rootElement.getNamespace() == Namespace.NO_NAMESPACE )
         {
-			writer = new FileWriter( file );
-			write( writer );
-			writer.close();
-		}
-		finally
-		{
-			IOUtils.closeQuietly( writer );
-		}
-	}
+            namespace = Namespace.getNamespace( "http://www.adobe.com/2006/flex-config" );
+            rootElement.setNamespace( namespace );
+        }
 
-	public void addOutput( File output )
-	{
-		if ( exists( "output" ) )
-		{
-			return;
-		}
+        removeCommentsAndSetNamespace( document.getContent() );
 
-		Element element = new Element( "output", namespace );
-		element.addContent( output.getAbsolutePath() );
-		rootElement.addContent( element );
-	}
+        if ( namespace == null )
+        {
+            namespace = rootElement.getNamespace();
+        }
+    }
 
-	public void addList( String[] list, String rootName, String childName )
-	{
-		if ( exists( rootName ) )
-		{
-			return;
-		}
+    public void write( Writer writer )
+        throws IOException
+    {
+        XMLOutputter outputter = new XMLOutputter();
+        Format format = Format.getPrettyFormat();
+        format.setIndent( "\t" );
+        outputter.setFormat( format );
 
-		Element element = new Element( rootName, namespace );
-		for ( String value : list )
-		{
-			Element child = new Element( childName, namespace );
-			child.addContent( value );
-			element.addContent( child );
-		}
-		rootElement.addContent( element );
-	}
+        outputter.output( document, writer );
+        writer.flush();
+    }
 
-	public void addList( File[] list, String rootName, String childName )
-	{
-		String[] paths = new String[list.length];
-		for ( int i = 0; i < list.length; i++ )
-		{
-			paths[ i ] = list[ i ].getAbsolutePath();
-		}
-		addList( paths, rootName, childName );
-	}
+    public void write( File file )
+        throws IOException
+    {
+        FileWriter writer = null;
+        try
+        {
+            writer = new FileWriter( file );
+            write( writer );
+            writer.close();
+        }
+        finally
+        {
+            IOUtils.closeQuietly( writer );
+        }
+    }
 
-	public void addIncludeFiles( String[] names, String[] paths )
-	{
-		for ( int i = 0; i < names.length; i++ )
-		{
-			Element element = new Element( "include-file", namespace );
+    public void addOutput( File output )
+    {
+        if ( exists( "output" ) )
+        {
+            return;
+        }
 
-			Element name = new Element( "name", namespace );
-			name.addContent( names[ i ] );
-			element.addContent( name );
+        Element element = new Element( "output", namespace );
+        element.addContent( output.getAbsolutePath() );
+        rootElement.addContent( element );
+    }
 
-			Element path = new Element( "path", namespace );
-			path.addContent( paths[ i ] );
-			element.addContent( path );
+    public void addList( String[] list, String rootName, String childName )
+    {
+        if ( exists( rootName ) )
+        {
+            return;
+        }
 
-			rootElement.addContent( element );
-		}
-	}
+        Element element = new Element( rootName, namespace );
+        for ( String value : list )
+        {
+            Element child = new Element( childName, namespace );
+            child.addContent( value );
+            element.addContent( child );
+        }
+        rootElement.addContent( element );
+    }
 
-	public void addEmptyLocale()
-	{
-		Element compilerElement = rootElement.getChild( "compiler", namespace );
-		if ( exists( "locale", compilerElement ) )
-		{
-			return;
-		}
-		compilerElement.addContent( new Element( "locale", namespace ) );
-	}
+    public void addList( File[] list, String rootName, String childName )
+    {
+        String[] paths = new String[list.length];
+        for ( int i = 0; i < list.length; i++ )
+        {
+            paths[i] = list[i].getAbsolutePath();
+        }
+        addList( paths, rootName, childName );
+    }
 
-	public void addSourcePath( String path )
-	{
-		Element sourceElement = rootElement.getChild( "compiler", namespace ).getChild( "source-path", namespace );
+    public void addIncludeFiles( String[] names, String[] paths )
+    {
+        for ( int i = 0; i < names.length; i++ )
+        {
+            Element element = new Element( "include-file", namespace );
 
-		Element pathElement = new Element( "path-element", namespace );
-		pathElement.addContent( path );
+            Element name = new Element( "name", namespace );
+            name.addContent( names[i] );
+            element.addContent( name );
 
-		sourceElement.addContent( pathElement );
-	}
+            Element path = new Element( "path", namespace );
+            path.addContent( paths[i] );
+            element.addContent( path );
 
-	public void addSourcePath( File file )
-	{
-		addSourcePath( file.getAbsolutePath() );
-	}
+            rootElement.addContent( element );
+        }
+    }
 
-	private void removeCommentsAndSetNamespace( List l )
-	{
-		for ( Iterator i = l.iterator(); i.hasNext(); )
-		{
-			Object node = i.next();
-			if ( node instanceof Comment )
-			{
-				i.remove();
-			}
-			else if ( node instanceof Element )
-			{
-				Element element = ( Element ) node;
-				if ( namespace != null )
-				{
-					element.setNamespace( namespace );
-				}
-				removeCommentsAndSetNamespace( element.getContent() );
-			}
-		}
-	}
+    public void addEmptyLocale()
+    {
+        Element compilerElement = rootElement.getChild( "compiler", namespace );
+        if ( exists( "locale", compilerElement ) )
+        {
+            return;
+        }
+        compilerElement.addContent( new Element( "locale", namespace ) );
+    }
 
-	private boolean exists( String name )
-	{
-		return rootElement.getChild( name, namespace ) != null;
-	}
+    public void addSourcePath( String path )
+    {
+        Element sourceElement = rootElement.getChild( "compiler", namespace ).getChild( "source-path", namespace );
 
-	private boolean exists( String name, Element parent )
-	{
-		return parent.getChild( name, namespace ) != null;
-	}
+        Element pathElement = new Element( "path-element", namespace );
+        pathElement.addContent( path );
+
+        sourceElement.addContent( pathElement );
+    }
+
+    public void addSourcePath( File file )
+    {
+        addSourcePath( file.getAbsolutePath() );
+    }
+
+    @SuppressWarnings( "unchecked" )
+    private void removeCommentsAndSetNamespace( List l )
+    {
+        for ( Iterator i = l.iterator(); i.hasNext(); )
+        {
+            Object node = i.next();
+            if ( node instanceof Comment )
+            {
+                i.remove();
+            }
+            else if ( node instanceof Element )
+            {
+                Element element = (Element) node;
+                if ( namespace != null )
+                {
+                    element.setNamespace( namespace );
+                }
+                removeCommentsAndSetNamespace( element.getContent() );
+            }
+        }
+    }
+
+    private boolean exists( String name )
+    {
+        return rootElement.getChild( name, namespace ) != null;
+    }
+
+    private boolean exists( String name, Element parent )
+    {
+        return parent.getChild( name, namespace ) != null;
+    }
 }
