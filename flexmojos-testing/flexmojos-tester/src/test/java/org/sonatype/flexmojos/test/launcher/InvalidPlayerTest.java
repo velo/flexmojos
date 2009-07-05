f861ff5076ee6aa0e184453f59813d811a5f0daa
Marvin Froeder <velo.br@gmail.com>
2009-07-05 19:30:31 +0000

    OPEN - issue FLEXMOJOS-139: Improve unit test support https://issues.sonatype.org/browse/FLEXMOJOS-139

--- flexmojos-testing/flexmojos-tester/src/test/java/org/sonatype/flexmojos/test/launcher/InvalidPlayerTest.java
+++ flexmojos-testing/flexmojos-tester/src/test/java/org/sonatype/flexmojos/test/launcher/InvalidPlayerTest.java
@@ -2,26 +2,18 @@ package org.sonatype.flexmojos.test.launcher;
 
 import static org.testng.Assert.fail;
 
-import org.codehaus.plexus.context.Context;
 import org.testng.annotations.Test;
 
 public class InvalidPlayerTest
     extends AbstractAsVmLauncherTest
 {
 
-    @Override
-    protected void customizeContext( Context context )
-    {
-        super.customizeContext( context );
-
-        context.put( "flashplayer.command", "invalid_flash_player" );
-    }
-
     @Test( timeOut = 20000 )
     public void invalidPlayer()
         throws Exception
     {
-        AsVmLauncher launcher = getContainer().lookup( AsVmLauncher.class );
+        set( launcher, "flashplayerCommand", "invalid_flash_player" );
+
         try
         {
             launcher.start( VALID_SWF );
