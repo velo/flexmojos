91c94929f0af6caf8cc42b7910183100ab31ccc3
Marvin Froeder <velo.br@gmail.com>
2009-07-06 14:12:49 +0000

    Ignoring Broken Pipe errors

--- flexmojos-testing/flexmojos-tester/src/test/java/org/sonatype/flexmojos/test/launcher/InvalidPlayerTest.java
+++ flexmojos-testing/flexmojos-tester/src/test/java/org/sonatype/flexmojos/test/launcher/InvalidPlayerTest.java
@@ -19,11 +19,20 @@ package org.sonatype.flexmojos.test.launcher;
 
 import static org.testng.Assert.fail;
 
+import org.codehaus.plexus.context.Context;
 import org.testng.annotations.Test;
 
 public class InvalidPlayerTest
     extends AbstractAsVmLauncherTest
 {
+
+    @Override
+    protected void customizeContext( Context context )
+    {
+        super.customizeContext( context );
+
+        context.put( "flashplayer.command", "invalid_flash_player" );
+    }
 
     @Test( timeOut = 20000 )
     public void invalidPlayer()
