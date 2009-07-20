6c9da61c575c2f08c290764662356313a433c624
Marvin Froeder <velo.br@gmail.com>
2009-07-20 12:14:26 +0000

    OPEN - issue FLEXMOJOS-145: Add new POM configuration to specify multiple Flex applications https://issues.sonatype.org/browse/FLEXMOJOS-145

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/flexbuilder/FlexbuilderMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/flexbuilder/FlexbuilderMojo.java
@@ -221,11 +221,21 @@ public class FlexbuilderMojo
     protected File[] sourcePaths;
 
     /**
-     * The file to be compiled. The path must be relative with source folder
+     * The file to be compiled. The path must be relative to the source folder.
      * 
      * @parameter
      */
     protected String sourceFile;
+
+    /**
+     * Additional application files.
+     * 
+     * The paths must be relative to the source folder.
+     * 
+     * @parameter
+     * @alias "applications"
+     */
+    protected List<String> additionalApplications;
 
     /**
      * Define the base path to locate resouce bundle files Accept some special tokens:
@@ -477,6 +487,8 @@ public class FlexbuilderMojo
         {
             File sourceFile = MavenUtils.resolveSourceFile( project, this.sourceFile );
             context.put( "mainApplication", sourceFile.getName() );
+            getAllApplications().add( 0, sourceFile.getName() );
+            context.put( "applications", getAllApplications() );
             context.put( "generateHtmlWrapper", generateHtmlWrapper );
         }
         else if ( SWC.equals( packaging ) )
@@ -510,6 +522,16 @@ public class FlexbuilderMojo
             }
         }
         return false;
+    }
+
+    private List<String> getAllApplications()
+    {
+        if ( additionalApplications == null )
+        {
+            additionalApplications = new ArrayList<String>(10);
+        }
+        
+        return additionalApplications;
     }
 
     private String getMainSources()
