2c92b8311866a0c7e47c4a672689ea309112720b
Marvin Froeder <velo.br@gmail.com>
2009-08-29 22:24:35 +0000

    OPEN - issue FLEXMOJOS-46: Mojo to generate manifest file https://issues.sonatype.org/browse/FLEXMOJOS-46

--- flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/concept/ManifestTest.java
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/concept/ManifestTest.java
@@ -1,5 +1,9 @@
 package org.sonatype.flexmojos.tests.concept;
 
+import java.io.File;
+
+import org.apache.maven.it.Verifier;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ManifestTest
@@ -10,7 +14,9 @@ public class ManifestTest
     public void manifestCreation()
         throws Exception
     {
-        standardConceptTester( "manifest" );
+        Verifier v = standardConceptTester( "manifest" );
+        File dir = new File( v.getBasedir() );
+        Assert.assertTrue( new File( dir, "target/manifest.xml" ).isFile() );
     }
 
 }
