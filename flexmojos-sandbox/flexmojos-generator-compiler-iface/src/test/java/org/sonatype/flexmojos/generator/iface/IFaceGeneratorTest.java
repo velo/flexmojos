a6bb480c70e36569fe91d2b29130a44a03f93d1f
Marvin Froeder <velo.br@gmail.com>
2009-10-05 11:41:34 +0000

    Preparing to the new compilation approach

---
+++ flexmojos-sandbox/flexmojos-generator-compiler-iface/src/test/java/org/sonatype/flexmojos/generator/iface/IFaceGeneratorTest.java
@@ -0,0 +1,92 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonatype.flexmojos.generator.iface;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
+import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.util.FileUtils;
+import org.sonatype.flexmojos.generator.api.GenerationException;
+import org.sonatype.flexmojos.generator.api.GenerationRequest;
+import org.sonatype.flexmojos.generator.api.Generator;
+
+import flex2.tools.CompcConfiguration;
+
+@SuppressWarnings( "restriction" )
+public class IFaceGeneratorTest
+    extends PlexusTestCase
+{
+
+    private Generator generator;
+
+    private File files;
+
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        this.generator = lookup( Generator.class, "internal-ifaces" );
+
+        files = new File( "./target/files" );
+        FileUtils.forceDelete( files );
+
+    }
+
+    @Override
+    protected void tearDown()
+        throws Exception
+    {
+        this.generator = null;
+
+        super.tearDown();
+    }
+
+    public void testGenerate()
+        throws GenerationException, IOException
+    {
+        GenerationRequest request = new GenerationRequest();
+        request.setTransientOutputFolder( files );
+        // request.addClass( Configuration.class.getName(), null );
+        request.addClass( CompcConfiguration.class.getName(), null );
+        // request.addClass( Builder.class.getName(), null );
+        // request.addClass( Library.class.getName(), null );
+        // request.addClass( Application.class.getName(), null );
+        request.setClassLoader( Thread.currentThread().getContextClassLoader() );
+
+        generator.generate( request );
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager( null, null, null );
+
+        Iterable<? extends JavaFileObject> compilationUnits1 =
+            fileManager.getJavaFileObjects( new File( files, "org/sonatype/flexmojos/compiler" ).listFiles() );
+        assertTrue( "Problems compiling files", compiler.getTask( null, fileManager, null, null, null,
+                                                                  compilationUnits1 ).call() );
+
+        fileManager.close();
+    }
+
+}
