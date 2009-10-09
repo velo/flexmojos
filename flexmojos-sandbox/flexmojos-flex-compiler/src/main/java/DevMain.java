1d66065f26d6b8f43ae50378fb417a186cf61f10
Marvin Froeder <velo.br@gmail.com>
2009-10-09 02:46:51 +0000

    Preparing to the new compilation approach

--- flexmojos-sandbox/flexmojos-flex-compiler/src/main/java/DevMain.java
+++ flexmojos-sandbox/flexmojos-flex-compiler/src/main/java/DevMain.java
@@ -15,38 +15,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.sonatype.flexmojos.compiler;
+import org.objectweb.asm.util.ASMifierClassVisitor;
 
-import org.codehaus.plexus.DefaultPlexusContainer;
-import org.codehaus.plexus.PlexusContainer;
-import org.codehaus.plexus.PlexusContainerException;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import api.API2;
 
-public class DefaultFlexCompilerTest
+public class DevMain
 {
-
-    private PlexusContainer plexus;
-
-    @BeforeClass
-    public void init()
-        throws PlexusContainerException
-    {
-        plexus = new DefaultPlexusContainer();
-    }
-
-    @Test
-    public void logTest()
+    public static void main( String[] args )
         throws Exception
     {
-        DefaultFlexCompiler compiler = (DefaultFlexCompiler) plexus.lookup( FlexCompiler.class );
-        MapLogger logger = new MapLogger();
-        compiler.enableLogging( logger );
-
-        compiler.compileSwc( null );
-
-        Assert.assertFalse( logger.getLogs().isEmpty() );
+        ASMifierClassVisitor.main( new String[] { API2.class.getName() } );
     }
 
 }
