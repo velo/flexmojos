00e1d8a1afda44d7a370ef9a1ce1e35c5a150ec8
Marvin Froeder <velo.br@gmail.com>
2009-11-29 13:38:08 +0000

    OPEN - issue FLEXMOJOS-226: Create a new goal that builds the SWF including all test libraries, useful for automation swfs https://issues.sonatype.org/browse/FLEXMOJOS-226

---
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/compiler/TestApplicationMojo.java
@@ -0,0 +1,73 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonatype.flexmojos.compiler;
+
+import static org.sonatype.flexmojos.common.FlexExtension.SWF;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Build the SWF including all TEST libraries.
+ * 
+ * @author Marvin Herman Froeder (velo.br@gmail.com)
+ * @since 3.5
+ * @goal test-swf
+ * @requiresDependencyResolution
+ */
+public class TestApplicationMojo
+    extends ApplicationMojo
+{
+
+    @Override
+    public void setUp()
+        throws MojoExecutionException, MojoFailureException
+    {
+        isSetProjectFile = false;
+
+        super.setUp();
+    }
+
+    @Override
+    protected void configure()
+        throws MojoExecutionException, MojoFailureException
+    {
+        super.configure();
+
+        // add test libraries
+        configuration.includeLibraries( getDependenciesPath( "test" ) );
+    }
+
+    @Override
+    protected void tearDown()
+        throws MojoExecutionException, MojoFailureException
+    {
+        super.tearDown();
+
+        projectHelper.attachArtifact( project, SWF, "test", getOutput() );
+    }
+
+    @Override
+    protected File getOutput()
+    {
+        return new File( build.getDirectory(), build.getFinalName() + "-test.swf" );
+    }
+
+}
