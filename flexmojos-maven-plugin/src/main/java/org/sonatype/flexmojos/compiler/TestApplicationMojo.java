4901856b0419eefe8ee4077e9e50921820045ed3
Marvin Froeder <velo.br@gmail.com>
2010-01-12 11:55:42 +0000

    OPEN - issue FLEXMOJOS-241: Update Maven site details for test-swf goal https://issues.sonatype.org/browse/FLEXMOJOS-241

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/compiler/TestApplicationMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/compiler/TestApplicationMojo.java
@@ -20,7 +20,7 @@ import org.apache.maven.plugin.MojoFailureException;
  * @author Marvin Herman Froeder (velo.br@gmail.com)
  * @since 3.5
  * @goal test-swf
- * @requiresDependencyResolution
+ * @requiresDependencyResolution test
  */
 public class TestApplicationMojo
     extends ApplicationMojo
