ca07ace7cd4925d6b26420f58e4470d776d1be10
Marvin Froeder <velo.br@gmail.com>
2010-03-14 16:57:32 +0000

    Flexmojos 4

--- flexmojos-sandbox/flexmojos-flex-compiler/src/test/java/org/sonatype/flexmojos/compiler/DefaultFlexCompilerTest.java
+++ flexmojos-sandbox/flexmojos-flex-compiler/src/test/java/org/sonatype/flexmojos/compiler/DefaultFlexCompilerTest.java
@@ -80,7 +80,7 @@ public class DefaultFlexCompilerTest
         when( cfg.getLoadConfig() ).thenReturn( new String[] {} );
         when( cfg.getOutput() ).thenReturn( output.getAbsolutePath() );
         when( cfg.getCompilerConfiguration() ).thenReturn( compilerCfg );
-        Assert.assertEquals( compiler.compileSwc( cfg ), 0 );
+        Assert.assertEquals( compiler.compileSwc( cfg, true ).getExitCode(), 0 );
     }
 
     @Test
@@ -98,7 +98,9 @@ public class DefaultFlexCompilerTest
         when( cfg.getLoadConfig() ).thenReturn( new String[] {} );
         when( cfg.getOutput() ).thenReturn( output.getAbsolutePath() );
         when( cfg.getCompilerConfiguration() ).thenReturn( compilerCfg );
-        Assert.assertEquals( compiler.compileSwf( new MxmlcConfigurationHolder( cfg, new File( as3, "main.as" ) ) ), 0 );
+        Assert.assertEquals(
+                             compiler.compileSwf( new MxmlcConfigurationHolder( cfg, new File( as3, "main.as" ) ), true ).getExitCode(),
+                             0 );
 
         Assert.assertTrue( output.exists(), logger.getLogs().toString() );
     }
