cbc97cd39b9f537735b4da07c3d9902ed2644931
Marvin Froeder <velo.br@gmail.com>
2010-03-27 18:34:37 +0000

    Flexmojos 4.0

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/test/TestCompilerMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/test/TestCompilerMojo.java
@@ -342,7 +342,7 @@ public class TestCompilerMojo
     }
 
     @SuppressWarnings( "unchecked" )
-    private Artifact getFlexmojosUnittestFrameworkIntegrationLibrary()
+    protected Artifact getFlexmojosUnittestFrameworkIntegrationLibrary()
     {
 
         if ( getDependency( groupId( "com.adobe.flexunit" ), artifactId( "flexunit" ),
@@ -382,10 +382,10 @@ public class TestCompilerMojo
         }
     }
 
-    private Artifact getFlexmojosTestArtifact( String artifactId )
+    protected Artifact getFlexmojosTestArtifact( String artifactId )
     {
         Artifact artifact =
-            super.resolve( "org.sonatype.flexmojos", artifactId, MavenUtils.getFlexMojosVersion(), null, "swc" );
+            resolve( "org.sonatype.flexmojos", artifactId, MavenUtils.getFlexMojosVersion(), null, "swc" );
 
         return artifact;
     }
