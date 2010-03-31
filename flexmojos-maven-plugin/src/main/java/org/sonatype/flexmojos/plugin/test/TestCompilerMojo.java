9643f36f0eec5f9ebf5232e97e2182dbbe05d19d
Marvin Froeder <velo.br@gmail.com>
2010-03-31 01:50:43 +0000

    Flexmojos 4.0 Add RSL support

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/test/TestCompilerMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/test/TestCompilerMojo.java
@@ -380,12 +380,6 @@ public class TestCompilerMojo
     }
 
     @Override
-    public String[] getRuntimeSharedLibraries()
-    {
-        return null;
-    }
-
-    @Override
     public IRuntimeSharedLibraryPath[] getRuntimeSharedLibraryPath()
     {
         return null;
