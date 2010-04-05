46166b5078ccc099a9b359028581f018cdf51cfc
Marvin Froeder <velo.br@gmail.com>
2010-04-05 11:15:11 +0000

    Flexmojos 4.0

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/MxmlcMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/MxmlcMojo.java
@@ -133,12 +133,6 @@ public class MxmlcMojo
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-        if ( !PathUtil.exist( getSourcePath() ) )
-        {
-            getLog().warn( "Skipping compiler, source path doesn't exist." );
-            return;
-        }
-
         executeCompiler( new MxmlcConfigurationHolder( this, getSourceFile() ), true );
 
         if ( localesRuntime != null )
