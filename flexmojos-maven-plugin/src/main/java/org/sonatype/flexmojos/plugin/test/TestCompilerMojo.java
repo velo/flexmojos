2abb90b453f5ad9a035f606b8749e2ed59b2162f
Marvin Froeder <velo.br@gmail.com>
2010-05-04 20:53:09 +0000

    Flexmojos 4.0

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/test/TestCompilerMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/test/TestCompilerMojo.java
@@ -266,10 +266,21 @@ public class TestCompilerMojo
 
         StringBuilder imports = new StringBuilder();
 
-        for ( String testClass : testClasses )
+        File[] sp = getSourcePath();
+        List<String> projectClasses = filterClasses( as3ClassesFileSet( sp ), sp );
+        for ( String testClass : projectClasses )
         {
             imports.append( "import " );
             imports.append( testClass );
+            imports.append( "; " );
+            if ( testClass.indexOf( '.' ) != -1 )
+            {
+                imports.append( testClass.substring( testClass.lastIndexOf( '.' ) + 1 ) );
+            }
+            else
+            {
+                imports.append( testClass );
+            }
             imports.append( ";" );
             imports.append( '\n' );
         }
@@ -304,7 +315,7 @@ public class TestCompilerMojo
     {
         return true;
     }
-    
+
     @Override
     public Boolean getOptimize()
     {
