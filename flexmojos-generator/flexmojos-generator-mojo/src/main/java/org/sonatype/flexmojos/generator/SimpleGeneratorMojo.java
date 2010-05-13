8111e2e982ce51a1cb477bc44f10474c3ae603bd
Marvin Froeder <velo.br@gmail.com>
2010-05-13 12:27:21 +0000

    Flexmojos 4.0 AIR unit test support

--- flexmojos-generator/flexmojos-generator-mojo/src/main/java/org/sonatype/flexmojos/generator/SimpleGeneratorMojo.java
+++ flexmojos-generator/flexmojos-generator-mojo/src/main/java/org/sonatype/flexmojos/generator/SimpleGeneratorMojo.java
@@ -72,22 +72,6 @@ public class SimpleGeneratorMojo
      * File to generate as3 file. If not defined assumes all classes must be included
      * 
      * @parameter
-     * @deprecated
-     */
-    private String[] includeClasses;
-
-    /**
-     * File to exclude from as3 generation. If not defined, assumes no exclusions
-     * 
-     * @parameter
-     * @deprecated
-     */
-    private String[] excludeClasses;
-
-    /**
-     * File to generate as3 file. If not defined assumes all classes must be included
-     * 
-     * @parameter
      */
     private String[] includeJavaClasses;
 
@@ -219,19 +203,7 @@ public class SimpleGeneratorMojo
     {
         if ( includeJavaClasses == null )
         {
-            if ( includeClasses != null )
-            {
-                includeJavaClasses = includeClasses;
-            }
-            else
-            {
-                includeJavaClasses = new String[] { "*" };
-            }
-        }
-
-        if ( excludeJavaClasses == null )
-        {
-            excludeJavaClasses = excludeClasses;
+            includeJavaClasses = new String[] { "*" };
         }
 
         if ( !outputDirectory.exists() )
