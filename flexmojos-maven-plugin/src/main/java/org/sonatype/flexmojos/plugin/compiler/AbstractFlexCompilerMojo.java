8aa6efa2b215da7f89b2563c12bbc8e38374359c
Marvin Froeder <velo.br@gmail.com>
2010-06-29 12:37:03 +0000

    Flexmojos 4.0 quick mode

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/AbstractFlexCompilerMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/AbstractFlexCompilerMojo.java
@@ -2972,7 +2972,7 @@ public abstract class AbstractFlexCompilerMojo<CFG, C extends AbstractFlexCompil
         if ( !artifact.isResolved() || artifact.getFile() == null || !artifact.getFile().exists() )
         {
             // Recompile, file doesn't exists
-            getLog().warn( "Can't find any older instaled version." );
+            getLog().warn( "Can't find any older installed version." );
             return true;
         }
 
@@ -3009,7 +3009,13 @@ public abstract class AbstractFlexCompilerMojo<CFG, C extends AbstractFlexCompil
         {
             try
             {
-                FileUtils.copyFile( artifact.getFile(), new File( getOutput() ) );
+				final File output = new File( getOutput() );
+
+                FileUtils.copyFile( artifact.getFile(), output );
+				
+				if( !output.setLastModified( artifact.getFile().lastModified() ) ) {
+					getLog().warn( "Could not set modified on copied artifact. Unnecessary rebuilds will occur." );
+				}
             }
             catch ( IOException e )
             {
