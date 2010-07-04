68ebf8093e1a892f26e1dc116877eaa959b7b3de
Marvin Froeder <velo.br@gmail.com>
2010-07-04 22:35:43 +0000

    Flexmojos 4.0 Aborted unforked ITs

--- flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/AbstractFlexMojosTests.java
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/AbstractFlexMojosTests.java
@@ -12,7 +12,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
