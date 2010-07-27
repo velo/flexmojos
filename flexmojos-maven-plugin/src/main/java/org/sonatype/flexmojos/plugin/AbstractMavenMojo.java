ada1a88db3ca639cc32983e92e869882d9dc4335
Marvin Froeder <velo.br@gmail.com>
2010-07-27 19:20:52 +0000

    Flexmojos 4 was ignoring missing locales, it must yell about it.

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/AbstractMavenMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/AbstractMavenMojo.java
@@ -386,6 +386,17 @@ public abstract class AbstractMavenMojo
     }
 
     @SuppressWarnings( "unchecked" )
+    public <E> E getFromPluginContext( String key )
+    {
+        Object valueHolder = getPluginContext().get( key );
+        if ( valueHolder instanceof ThreadLocal )
+        {
+            return ( (ThreadLocal<E>) valueHolder ).get();
+        }
+        return (E) valueHolder;
+    }
+
+    @SuppressWarnings( "unchecked" )
     public boolean getIsAirProject()
     {
         return getDependency( groupId( FRAMEWORK_GROUP_ID ), artifactId( AIR_GLOBAL ), type( SWC ) ) != null;
@@ -472,6 +483,18 @@ public abstract class AbstractMavenMojo
     public boolean isSkip()
     {
         return skip;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public <E> void putPluginContext( String key, E value )
+    {
+        Object valueHolder = getPluginContext().get( key );
+        if ( !( valueHolder instanceof ThreadLocal ) )
+        {
+            valueHolder = new ThreadLocal<E>();
+            getPluginContext().put( key, valueHolder );
+        }
+        ( (ThreadLocal<E>) valueHolder ).set( value );
     }
 
     protected Artifact resolve( String groupId, String artifactId, String version, String classifier, String type )
@@ -587,30 +610,6 @@ public abstract class AbstractMavenMojo
         {
             checkResult( result );
         }
-    }
-    
-
-    @SuppressWarnings( "unchecked" )
-    public <E> void putPluginContext( String key, E value )
-    {
-        Object valueHolder = getPluginContext().get( key );
-        if ( !( valueHolder instanceof ThreadLocal ) )
-        {
-            valueHolder = new ThreadLocal<E>();
-            getPluginContext().put( key, valueHolder );
-        }
-        ( (ThreadLocal<E>) valueHolder ).set( value );
-    }
-
-    @SuppressWarnings( "unchecked" )
-    public <E> E getFromPluginContext( String key )
-    {
-        Object valueHolder = getPluginContext().get( key );
-        if ( valueHolder instanceof ThreadLocal )
-        {
-            return ( (ThreadLocal<E>) valueHolder ).get();
-        }
-        return (E) valueHolder;
     }
 
 }
