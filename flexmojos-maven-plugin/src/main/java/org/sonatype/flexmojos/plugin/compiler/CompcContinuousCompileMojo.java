81ff7628e6c2da95856d221a5c31ff23ff584d40
Marvin Froeder <velo.br@gmail.com>
2010-09-03 14:37:23 +0000

    Created continuos compilation goals, thanks to Joa Ebert

---
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/CompcContinuousCompileMojo.java
@@ -0,0 +1,67 @@
+package org.sonatype.flexmojos.plugin.compiler;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @since 4.0
+ * @goal cc-swc
+ * @requiresDependencyResolution compile
+ * @phase compile
+ * @configurator flexmojos
+ * @threadSafe
+ * @author Joa Ebert
+ */
+public class CompcContinuousCompileMojo
+    extends CompcMojo
+{
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        //
+        // We have ot set quick to true since isCompilationRequired does a check
+        // only if quick has been set to true.
+        //
+
+        super.quick = true;
+
+        try
+        {
+            showInfo();
+
+            while ( !Thread.interrupted() )
+            {
+                if ( isCompilationRequired() )
+                {
+                    //
+                    // We have to compile so let's hand the job to
+                    // the CompcMojo implementation and do the actual work.
+                    //
+
+                    super.execute();
+
+                    showInfo();
+
+                    Thread.sleep( 4000L );
+                }
+                else
+                {
+                    Thread.sleep( 2000L );
+                }
+            }
+        }
+        catch ( final InterruptedException interruptException )
+        {
+            // nothing to do here
+        }
+    }
+
+    protected void showInfo()
+    {
+        getLog().info( "Waiting for files to compile ..." );
+    }
+}
