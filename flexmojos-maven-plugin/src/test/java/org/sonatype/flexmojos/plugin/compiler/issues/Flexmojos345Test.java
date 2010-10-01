0496d8690b83c77d5440faf432f81a2ae80cfc7f
Marvin Froeder <velo.br@gmail.com>
2010-10-01 19:49:15 -0300

    Fix FLEXMOJOS-345 -  Backslash vs. forward slash in catalog.xml

---
+++ flexmojos-maven-plugin/src/test/java/org/sonatype/flexmojos/plugin/compiler/issues/Flexmojos345Test.java
@@ -0,0 +1,50 @@
+package org.sonatype.flexmojos.plugin.compiler.issues;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.sonatype.flexmojos.compiler.IIncludeFile;
+import org.sonatype.flexmojos.plugin.compiler.CompcMojo;
+import org.sonatype.flexmojos.plugin.compiler.attributes.converter.SimplifiablePattern;
+import org.testng.annotations.Test;
+
+public class Flexmojos345Test
+{
+
+    @Test
+    public void checkFileInclusions()
+        throws Exception
+    {
+        CompcMojo m = new CompcMojo()
+        {
+            @Override
+            public IIncludeFile[] getIncludeFile()
+            {
+                includeFiles = new SimplifiablePattern();
+                includeFiles.addInclude( "abc\\cba\\test" );
+                return super.getIncludeFile();
+            }
+
+            @Override
+            protected List<File> getResourcesTargetDirectories()
+            {
+                return Arrays.asList( new File( "." ) );
+            }
+        };
+
+        IIncludeFile[] files = m.getIncludeFile();
+
+        for ( IIncludeFile file : files )
+        {
+            assertThat( file.name(), not( containsString( "\\" ) ) );
+        }
+    }
+
+}
