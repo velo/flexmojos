bbb172c0f6f8d459c39c63470de2c7da012c3b8b
Marvin Froeder <velo.br@gmail.com>
2010-10-09 11:03:14 -0300

    Fixing Issue0152Test

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/MxmlcMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/compiler/MxmlcMojo.java
@@ -80,6 +80,11 @@ public class MxmlcMojo
     private Module[] modules;
 
     /**
+     * @parameter expression="${flex.modulesLoadExterns}" default-value="true"
+     */
+    private boolean modulesLoadExterns;
+
+    /**
      * DOCME Another, undocumented by adobe
      * <p>
      * Equivalent to -projector
@@ -152,6 +157,11 @@ public class MxmlcMojo
 
             for ( Module module : modules )
             {
+                if ( module.isOptimize() == null )
+                {
+                    module.setOptimize( modulesLoadExterns );
+                }
+
                 File moduleSource =
                     SourceFileResolver.resolveSourceFile( project.getCompileSourceRoots(), module.getSourceFile() );
 
