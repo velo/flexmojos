4594676f6fa66e9ef016c3cc33d95a37e19ce4fc
Marvin Froeder <velo.br@gmail.com>
2010-10-09 12:01:57 -0300

    Fix generator related tests

--- flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/concept/GeneratorTest.java
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/concept/GeneratorTest.java
@@ -10,14 +10,32 @@ public class GeneratorTest
     public void testGenerationGranite1()
         throws Exception
     {
-        standardConceptTester( "simple-generation", "-DgeneratorToUse=graniteds1" );
+        standardConceptTester( "simple-generation", "-DgeneratorArtifact=flexmojos-generator-graniteds-1.1.0",
+                               "-DgeneratorToUse=graniteds1" );
     }
 
     @Test( groups = { "generator" } )
     public void testGenerationGranite2()
         throws Exception
     {
-        standardConceptTester( "simple-generation", "-DgeneratorToUse=graniteds2" );
+        standardConceptTester( "simple-generation", "-DgeneratorArtifact=flexmojos-generator-graniteds-2.0.0",
+                               "-DgeneratorToUse=graniteds2" );
+    }
+
+    @Test( groups = { "generator" } )
+    public void testGenerationGranite21()
+        throws Exception
+    {
+        standardConceptTester( "simple-generation", "-DgeneratorArtifact=flexmojos-generator-graniteds-2.1.0",
+                               "-DgeneratorToUse=graniteds21" );
+    }
+    
+    @Test( groups = { "generator" } )
+    public void testGenerationGranite22()
+    throws Exception
+    {
+        standardConceptTester( "simple-generation", "-DgeneratorArtifact=flexmojos-generator-graniteds-2.2.0",
+        "-DgeneratorToUse=graniteds22" );
     }
 
 }
