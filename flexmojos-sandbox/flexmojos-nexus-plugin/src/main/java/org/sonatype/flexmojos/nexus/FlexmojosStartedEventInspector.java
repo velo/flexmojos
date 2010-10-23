fbc0f4d93d03d0bb980e04414c2f3ea28dc47a59
Marvin Froeder <velo.br@gmail.com>
2010-10-23 18:45:09 -0200

    Creating a nexus-plugin to make nexus users life even easier

---
+++ flexmojos-sandbox/flexmojos-nexus-plugin/src/main/java/org/sonatype/flexmojos/nexus/FlexmojosStartedEventInspector.java
@@ -0,0 +1,155 @@
+package org.sonatype.flexmojos.nexus;
+
+import java.io.IOException;
+import static java.util.Arrays.*;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.sonatype.configuration.ConfigurationException;
+import org.sonatype.nexus.configuration.ConfigurationIdGenerator;
+import org.sonatype.nexus.configuration.application.NexusConfiguration;
+import org.sonatype.nexus.proxy.NoSuchRepositoryException;
+import org.sonatype.nexus.proxy.events.AbstractEventInspector;
+import org.sonatype.nexus.proxy.events.EventInspector;
+import org.sonatype.nexus.proxy.events.NexusStartedEvent;
+import org.sonatype.nexus.proxy.mapping.RepositoryPathMapping;
+import org.sonatype.nexus.proxy.mapping.RequestRepositoryMapper;
+import org.sonatype.nexus.proxy.mapping.RepositoryPathMapping.MappingType;
+import org.sonatype.nexus.proxy.maven.maven2.Maven2ContentClass;
+import org.sonatype.nexus.proxy.registry.RepositoryRegistry;
+import org.sonatype.nexus.proxy.repository.GroupRepository;
+import org.sonatype.nexus.proxy.repository.Repository;
+import org.sonatype.nexus.templates.TemplateProvider;
+import org.sonatype.nexus.templates.repository.DefaultRepositoryTemplateProvider;
+import org.sonatype.nexus.templates.repository.RepositoryTemplate;
+import org.sonatype.plexus.appevents.Event;
+
+@Component( role = EventInspector.class, hint = "FlexmojosStartedEventInspector" )
+public class FlexmojosStartedEventInspector
+    extends AbstractEventInspector
+    implements EventInspector
+{
+
+    @Requirement
+    private RequestRepositoryMapper repositoryMapper;
+
+    @Requirement( role = RepositoryRegistry.class )
+    private RepositoryRegistry repositoryRegistry;
+
+    @Requirement( role = TemplateProvider.class )
+    private TemplateProvider templateProvider;
+
+    @Requirement
+    private NexusConfiguration nexusConfiguration;
+
+    @Requirement
+    private ConfigurationIdGenerator idGenerator;
+
+    public boolean accepts( Event<?> evt )
+    {
+        return evt instanceof NexusStartedEvent;
+    }
+
+    public void inspect( Event<?> evt )
+    {
+        try
+        {
+            repositoryRegistry.getRepository( "flexmojos" );
+        }
+        catch ( NoSuchRepositoryException e )
+        {
+            // do not exists, creating it!
+            Repository repo = createFlexmojosRepository();
+            if ( repo != null )
+            {
+                GroupRepository group = setupPublicGroup( repo );
+                if ( group != null )
+                {
+                    setupRouting( group, repo );
+                }
+            }
+
+            try
+            {
+                nexusConfiguration.saveConfiguration();
+            }
+            catch ( IOException ex )
+            {
+                getLogger().error( "Unable to save flexmojos repository setup", ex );
+            }
+
+        }
+    }
+
+    private GroupRepository setupPublicGroup( Repository repo )
+    {
+        GroupRepository publicGroup;
+        try
+        {
+            publicGroup = repositoryRegistry.getRepositoryWithFacet( "public", GroupRepository.class );
+        }
+        catch ( NoSuchRepositoryException e )
+        {
+            getLogger().error( "Public group not found", e );
+            return null;
+        }
+        try
+        {
+            publicGroup.addMemberRepositoryId( repo.getId() );
+            return publicGroup;
+        }
+        catch ( Exception e )
+        {
+            getLogger().error( "Unable to setup flexmojos repository properly", e );
+            return null;
+        }
+
+    }
+
+    private void setupRouting( GroupRepository group, Repository repo )
+    {
+        try
+        {
+            // to best performance go to flexmojos repo to get adobe and flexmojos artifacts
+            repositoryMapper.addMapping( new RepositoryPathMapping( idGenerator.generateId(), MappingType.INCLUSION,
+                                                                    group.getId(),
+                                                                    asList( ".*/org/sonatype/flexmojos/.*" ),
+                                                                    asList( repo.getId() ) ) );
+            repositoryMapper.addMapping( new RepositoryPathMapping( idGenerator.generateId(), MappingType.INCLUSION,
+                                                                    group.getId(), asList( ".*/com/adobe/.*" ),
+                                                                    asList( repo.getId() ) ) );
+            // flexmojos repo do not publish sources artifacts
+            repositoryMapper.addMapping( new RepositoryPathMapping( idGenerator.generateId(), MappingType.BLOCKING,
+                                                                    group.getId(),
+                                                                    asList( ".*/com/adobe/.*-sources.jar" ), null ) );
+        }
+        catch ( ConfigurationException e )
+        {
+            getLogger().error( "Unable to setup flexmojos routing properly", e );
+        }
+    }
+
+    private Repository createFlexmojosRepository()
+    {
+        getLogger().info( "Default Flexmojos repository is missing, creating it." );
+        try
+        {
+            RepositoryTemplate template =
+                (RepositoryTemplate) templateProvider.getTemplateById( "default_proxy_release" );
+
+            template.getConfigurableRepository().setId( "flexmojos" );
+            template.getConfigurableRepository().setName( "Flexmojos Repository" );
+
+            Repository repo = template.create();
+            repositoryRegistry.addRepository( repo );
+
+            return repo;
+        }
+        catch ( Exception e )
+        {
+            getLogger().error( "Unable to setup flexmojos repository properly", e );
+            return null;
+        }
+    }
+
+}
---
+++ flexmojos-sandbox/flexmojos-nexus-plugin/src/test/java/org/sonatype/flexmojos/nexus/CheckRepositoryIT.java
@@ -0,0 +1,46 @@
+package org.sonatype.flexmojos.nexus;
+
+import static ch.lambdaj.Lambda.having;
+import static ch.lambdaj.Lambda.on;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.testng.Assert.assertNotNull;
+
+import org.hamcrest.Matcher;
+import org.restlet.data.MediaType;
+import org.sonatype.nexus.integrationtests.AbstractNexusProxyIntegrationTest;
+import org.sonatype.nexus.rest.model.RepositoryBaseResource;
+import org.sonatype.nexus.rest.model.RepositoryGroupMemberRepository;
+import org.sonatype.nexus.rest.model.RepositoryGroupResource;
+import org.sonatype.nexus.test.utils.GroupMessageUtil;
+import org.sonatype.nexus.test.utils.RepositoryMessageUtil;
+import org.sonatype.nexus.test.utils.XStreamFactory;
+import org.testng.annotations.Test;
+
+public class CheckRepositoryIT
+    extends AbstractNexusProxyIntegrationTest
+{
+
+    @SuppressWarnings( "unchecked" )
+    @Test
+    public void download()
+        throws Exception
+    {
+
+        RepositoryMessageUtil repoUtil =
+            new RepositoryMessageUtil( this, XStreamFactory.getXmlXStream(), MediaType.APPLICATION_XML,
+                                       getRepositoryTypeRegistry() );
+        GroupMessageUtil groupUtil =
+            new GroupMessageUtil( this, XStreamFactory.getXmlXStream(), MediaType.APPLICATION_XML );
+
+        RepositoryBaseResource flexmojos = repoUtil.getRepository( "flexmojos" );
+        assertNotNull( flexmojos );
+
+        RepositoryGroupResource publicGroup = groupUtil.getGroup( "public" );
+        assertNotNull( publicGroup );
+        Matcher<?> matcher =
+            hasItem( having( on( RepositoryGroupMemberRepository.class ).getId(), equalTo( flexmojos.getId() ) ) );
+        assertThat( publicGroup.getRepositories(), (Matcher<Iterable<RepositoryGroupMemberRepository>>) matcher );
+    }
+}
