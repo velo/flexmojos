7cbadfd5ee8d342cb138321b72cbc5431d3c5eee
Marvin Froeder <velo.br@gmail.com>
2010-11-05 20:01:10 -0200

    Add some context to configurators

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/configuration/ConfigurationMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/configuration/ConfigurationMojo.java
@@ -19,6 +19,7 @@ package org.sonatype.flexmojos.plugin.configuration;
 
 import static org.sonatype.flexmojos.plugin.common.FlexExtension.SWC;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -86,6 +87,20 @@ public class ConfigurationMojo
      */
     private String projector;
 
+    /**
+     * Extra parameters that will be injected onto configurator
+     * 
+     * <pre>
+     * &lt;parameters&gt;
+     *   &lt;extra&gt;???&lt;/extra&gt;
+     *   &lt;plus&gt;???&lt;/plus&gt;
+     * &lt;/parameters&gt;
+     * </pre>
+     * 
+     * @parameters
+     */
+    private Map<String, Object> parameters;
+
     @Override
     public Result doCompile( ICompcConfiguration cfg, boolean synchronize )
         throws Exception
@@ -122,13 +137,19 @@ public class ConfigurationMojo
             }
         }
 
+        if ( parameters == null )
+        {
+            parameters = new LinkedHashMap<String, Object>();
+        }
+        parameters.put( "project", project );
+
         if ( SWC.equals( getProjectType() ) )
         {
-            cfg.buildConfiguration( (ICompcConfiguration) this );
+            cfg.buildConfiguration( (ICompcConfiguration) this, parameters );
         }
         else
         {
-            cfg.buildConfiguration( (ICommandLineConfiguration) this );
+            cfg.buildConfiguration( (ICommandLineConfiguration) this, parameters );
         }
     }
 
