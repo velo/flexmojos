8b9f44bc7f165934307f7de7a09c93334a2de3e0
Marvin Froeder <velo.br@gmail.com>
2010-11-05 20:01:43 -0200

    Add some context to configurators

--- flexmojos-sandbox/flexmojos-sample-configurator/src/main/java/org/sonatype/flexmojos/configurator/sample/SampleConfigurator.java
+++ flexmojos-sandbox/flexmojos-sample-configurator/src/main/java/org/sonatype/flexmojos/configurator/sample/SampleConfigurator.java
@@ -1,5 +1,7 @@
 package org.sonatype.flexmojos.configurator.sample;
 
+import java.util.Map;
+
 import org.sonatype.flexmojos.compiler.ICommandLineConfiguration;
 import org.sonatype.flexmojos.compiler.ICompcConfiguration;
 import org.sonatype.flexmojos.configurator.Configurator;
@@ -8,13 +10,13 @@ public class SampleConfigurator
     implements Configurator
 {
 
-    public void buildConfiguration( ICommandLineConfiguration swf )
+    public void buildConfiguration( ICommandLineConfiguration swf, Map<String, Object> parameters )
     {
         System.out.println( "Running configurator for a SWF project." );
         System.out.println( swf.getOutput() );
     }
 
-    public void buildConfiguration( ICompcConfiguration swc )
+    public void buildConfiguration( ICompcConfiguration swc, Map<String, Object> parameters )
     {
         System.out.println( "Running configurator for a SWC project." );
         System.out.println( swc.getOutput() );
