bc1dd809896799ce13bd27ee5d0aec139914cc0d
Marvin Froeder <velo.br@gmail.com>
2010-12-01 17:18:38 -0200

    Create a test to cover test-swf goal

---
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/org/sonatype/flexmojos/tests/concept/TestSwfTest.java
@@ -0,0 +1,63 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonatype.flexmojos.tests.concept;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.it.VerificationException;
+import org.hamcrest.Matcher;
+import org.sonatype.flexmojos.test.FMVerifier;
+import org.sonatype.flexmojos.tests.AbstractFlexMojosTests;
+import org.sonatype.flexmojos.tests.matcher.ClassMatcher;
+import org.testng.annotations.Test;
+
+public class TestSwfTest
+    extends AbstractFlexMojosTests
+{
+
+    @SuppressWarnings( "all" )
+    private static final Matcher<File> TEST_CLASS = ClassMatcher.hasClass( "flexunit.framework:TestSuite" );
+
+    @Test
+    public void testSwf()
+        throws Exception
+    {
+        File main = compile( "flexmojos:compile-swf", "hello-world-1.0-SNAPSHOT.swf" );
+        assertThat( main, not( TEST_CLASS ) );
+
+        main = compile( "flexmojos:test-swf", "hello-world-1.0-SNAPSHOT-test.swf" );
+        assertThat( main, TEST_CLASS );
+    }
+
+    private File compile( String goal, String file )
+        throws VerificationException, IOException, Exception
+    {
+        FMVerifier v = test( getProject( "intro/hello-world" ), goal, "-DisIt=true" );
+        String dir = v.getBasedir();
+
+        File target = new File( dir, "target" );
+        File main = new File( target, file );
+
+        assertSeftExit( main, 3539, v );
+        return main;
+    }
+}
---
+++ flexmojos-testing/flexmojos-test-harness/src/test/scala/org/sonatype/flexmojos/tests/matcher/ClassMatcher.scala
@@ -0,0 +1,65 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonatype.flexmojos.tests.matcher
+import apparat.abc._
+import apparat.bytecode._
+import apparat.swf._
+import java.io.{
+  File => JFile
+}
+import org.hamcrest.{ TypeSafeMatcher, Description, Matcher }
+
+object ClassMatcher {
+  def hasClass(classname: String): Matcher[JFile] = {
+    return new ClassMatcher(classname);
+  }
+}
+class ClassMatcher(classnames: String*) extends TypeSafeMatcher[JFile] {
+
+  private var fileTested: JFile = null;
+  private var found = new StringBuilder();
+
+  def matchesSafely(file: JFile): Boolean = {
+    fileTested = file;
+    for {
+      tag <- Swf fromFile file
+      abc <- Abc fromTag tag
+      nominal <- abc.types
+    } {
+      var foundName = nominal.name.namespace.name.name + ":" + nominal.name.name.name;
+      if (classnames contains foundName) {
+        return true;
+      } else {
+        found append foundName
+        found append ", "
+      }
+    }
+    false;
+  }
+
+  def describeTo(desc: Description): Unit = {
+    desc.appendText(" contains ");
+    for (classname <- classnames) {
+      desc.appendValue(classname);
+      desc.appendText(", ");
+    }
+    desc.appendValue(" instead found ");
+    desc.appendText(found.toString)
+  }
+
+}
