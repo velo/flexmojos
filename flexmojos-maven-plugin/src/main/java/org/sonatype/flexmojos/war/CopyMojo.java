602698100e034e757dfa2e10e6b8ce99661ab1ff
Roberto Lo Giacco <rlogiacco@gmail.com>
2011-02-15 00:05:07 +0000

    fixed version support on `copy-flex-resources` goal

--- flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/war/CopyMojo.java
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/war/CopyMojo.java
@@ -385,7 +385,7 @@ public class CopyMojo
             {
                 extension = SWZ;
             }
-            if ( rslArtifact.getVersionRange() != null )
+            if ( rslArtifact.getVersion() == null )
             {
                 try 
                 {
