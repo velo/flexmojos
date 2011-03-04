dee4a3124039fc91e508f63b4247bbecf813c090
Marvin Froeder <velo.br@gmail.com>
2011-03-04 00:33:51 -0300

    Improve air native packaging support

---
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/air/packager/FlexmojosAIRPackager.java
@@ -0,0 +1,97 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonatype.flexmojos.plugin.air.packager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+
+import com.adobe.air.AIRPackager;
+import com.adobe.air.Listener;
+
+public class FlexmojosAIRPackager
+    implements IPackager
+{
+
+    private final AIRPackager packager;
+
+    public FlexmojosAIRPackager()
+    {
+        super();
+        this.packager = new AIRPackager();
+    }
+
+    public void setOutput( File output )
+        throws FileNotFoundException, IOException
+    {
+        this.packager.setOutput( output );
+    }
+
+    public void setDescriptor( File airDescriptor )
+    {
+        this.packager.setDescriptor( airDescriptor );
+    }
+
+    public void setPrivateKey( PrivateKey key )
+    {
+        this.packager.setPrivateKey( key );
+    }
+
+    public void setSignerCertificate( Certificate certificate )
+        throws CertificateException
+    {
+        this.packager.setSignerCertificate( certificate );
+    }
+
+    public void setCertificateChain( Certificate[] certificateChain )
+        throws CertificateException
+    {
+        this.packager.setCertificateChain( certificateChain );
+    }
+
+    public void setTimestampURL( String url )
+    {
+        this.packager.setTimestampURL( url );
+    }
+
+    public void addSourceWithPath( File source, String path )
+    {
+        this.packager.addSourceWithPath( source, path );
+    }
+
+    public void setListener( Listener listener )
+    {
+        this.packager.setListener( listener );
+    }
+
+    public void createPackage()
+        throws GeneralSecurityException, IOException
+    {
+        this.packager.createPackage();
+    }
+
+    public void close()
+    {
+        this.packager.close();
+    }
+
+}
---
+++ flexmojos-maven-plugin/src/main/java/org/sonatype/flexmojos/plugin/air/packager/IPackager.java
@@ -0,0 +1,56 @@
+/**
+ * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sonatype.flexmojos.plugin.air.packager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+
+import com.adobe.air.Listener;
+
+public interface IPackager
+{
+
+    void setOutput( File output )
+        throws FileNotFoundException, IOException;
+
+    void setDescriptor( File airDescriptor );
+
+    void setPrivateKey( PrivateKey key );
+
+    void setSignerCertificate( Certificate certificate )
+        throws CertificateException;
+
+    void setCertificateChain( Certificate[] certificateChain )
+        throws CertificateException;
+
+    void setTimestampURL( String string );
+
+    void addSourceWithPath( File source, String path );
+
+    void setListener( Listener listener );
+
+    void createPackage()
+        throws GeneralSecurityException, IOException;
+
+    void close();
+}
