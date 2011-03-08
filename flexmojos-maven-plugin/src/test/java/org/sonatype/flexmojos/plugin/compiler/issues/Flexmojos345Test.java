8a367db9a693d5016834b7b3f0e6e5cfdc63176f
Christofer Dutz <christofer.dutz@c-ware.de>
2011-03-08 17:31:52 +0100

    Fixed a test to make the build work under linux environments.

--- flexmojos-maven-plugin/src/test/java/org/sonatype/flexmojos/plugin/compiler/issues/Flexmojos345Test.java
+++ flexmojos-maven-plugin/src/test/java/org/sonatype/flexmojos/plugin/compiler/issues/Flexmojos345Test.java
@@ -29,6 +29,7 @@ import java.util.List;
 import org.sonatype.flexmojos.compiler.IIncludeFile;
 import org.sonatype.flexmojos.plugin.compiler.CompcMojo;
 import org.sonatype.flexmojos.plugin.compiler.attributes.SimplifiablePattern;
+import org.sonatype.flexmojos.util.OSUtils;
 import org.testng.annotations.Test;
 
 public class Flexmojos345Test
@@ -38,28 +39,31 @@ public class Flexmojos345Test
     public void checkFileInclusions()
         throws Exception
     {
-        CompcMojo m = new CompcMojo()
-        {
-            @Override
-            public IIncludeFile[] getIncludeFile()
-            {
-                includeFiles = new SimplifiablePattern();
-                includeFiles.addInclude( "abc/cba/test" );
-                return super.getIncludeFile();
-            }
+        // This problem only occurs on Windows machines, so we can only test for it on them.
+		if(OSUtils.isWindows()) {
+			CompcMojo m = new CompcMojo()
+			{
+				@Override
+				public IIncludeFile[] getIncludeFile()
+				{
+					includeFiles = new SimplifiablePattern();
+					includeFiles.addInclude( "abc\\cba\\test" );
+					return super.getIncludeFile();
+				}
 
-            @Override
-            protected List<File> getResourcesTargetDirectories()
-            {
-                return Arrays.asList( file( "./target/test-classes" ) );
-            }
-        };
+				@Override
+				protected List<File> getResourcesTargetDirectories()
+				{
+					return Arrays.asList( file( "./target/test-classes" ) );
+				}
+			};
 
-        IIncludeFile[] files = m.getIncludeFile();
+			IIncludeFile[] files = m.getIncludeFile();
 
-        for ( IIncludeFile file : files )
-        {
-            assertThat( file.name(), not( containsString( "\\" ) ) );
+			for ( IIncludeFile file : files )
+			{
+				assertThat( file.name(), not( containsString( "\\" ) ) );
+			}
         }
     }
 
