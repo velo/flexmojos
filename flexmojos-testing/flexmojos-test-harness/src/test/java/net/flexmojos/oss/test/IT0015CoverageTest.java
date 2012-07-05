2adcb18926b78e03f7e3c6b78e5af396a202e9c4
Christofer Dutz <christofer.dutz@c-ware.de>
2012-07-05 10:32:11 +0200

    In the testsuite several tests are failling because of errors in the generated asdoc - As the new mxmlc ASDoc generator is a pile of crap, I updated the code-comments of the classes that made the build fail to contain encoded xml entities.

--- flexmojos-testing/flexmojos-test-harness/src/test/java/net/flexmojos/oss/test/IT0015CoverageTest.java
+++ flexmojos-testing/flexmojos-test-harness/src/test/java/net/flexmojos/oss/test/IT0015CoverageTest.java
@@ -1,6 +1,7 @@
 /**
- * Flexmojos is a set of maven goals to allow maven users to compile, optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
- * Copyright (C) 2008-2012  Marvin Froeder <marvin@flexmojos.net>
+ * Flexmojos is a set of maven goals to allow maven users to compile,
+ * optimize and test Flex SWF, Flex SWC, Air SWF and Air SWC.
+ * Copyright (C) 2008-2012  Marvin Froeder &lt;marvin (at) flexmojos.net&gt;
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
  */
 package net.flexmojos.oss.test;
 
